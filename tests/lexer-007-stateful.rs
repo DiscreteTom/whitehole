57a50183bd43b08fcbff2e64153f00b28defa26a
DiscreteTom <discrete_tom@outlook.com>
2024-01-25 22:26:59 +0800

    tests(lexer): update 007 stateful

--- tests/lexer-007-stateful.rs
+++ tests/lexer-007-stateful.rs
@@ -32,14 +32,20 @@ fn stateful_lexer() {
     .build("123123");
 
   // by default `state.reject` is `false`
-  assert_eq!(lexer.core().state().reject, false);
+  assert_eq!(lexer.action_state().reject, false);
 
   // the first lex should be accepted
   assert_ne!(lexer.lex().digested, 0);
 
   // and the action state will be changed
-  assert_eq!(lexer.core().state().reject, true);
+  assert_eq!(lexer.action_state().reject, true);
 
   // then the second lex should be rejected
   assert_eq!(lexer.lex().digested, 0);
+
+  // besides, you can set the action_state directly
+  lexer.action_state_mut().reject = false;
+  assert_eq!(lexer.action_state().reject, false);
+  *lexer.action_state_mut() = MyState { reject: true };
+  assert_eq!(lexer.action_state().reject, true);
 }
