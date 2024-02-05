10b3b575f8a727798685d1ed3f4195261e54de6c
DiscreteTom <discrete_tom@outlook.com>
2024-02-05 18:38:28 +0800

    refactor(parser): use GrammarRule as State's candidate

--- src/parser/elr/dfa/state.rs
+++ src/parser/elr/dfa/state.rs
@@ -1,6 +1,58 @@
-use super::candidate::Candidate;
-use crate::lexer::token::TokenKind;
+use crate::{
+  lexer::{token::TokenKind, trimmed::TrimmedLexer},
+  parser::{ast::TNode, elr::grammar::grammar_rule::GrammarRule},
+};
+use std::{cell::RefCell, collections::HashSet, rc::Rc};
 
-pub struct State<'gr, 'grammar, 'candidate, TKind: TokenKind, NTKind> {
-  candidates: Vec<&'candidate Candidate<'grammar, 'gr, TKind, NTKind>>,
+pub struct State<TKind: TokenKind, NTKind: TokenKind> {
+  candidates: Vec<Rc<GrammarRule<TKind, NTKind>>>,
+  digested: usize,
+}
+
+impl<TKind: TokenKind, NTKind: TokenKind> State<TKind, NTKind> {
+  pub fn try_lex<
+    'buffer,
+    ASTData,
+    ErrorType,
+    LexerActionState: Default + Clone,
+    LexerErrorType,
+    Global,
+  >(
+    &self,
+    lexer: &TrimmedLexer<'buffer, TKind, LexerActionState, LexerErrorType>,
+    // TODO: add param token_ast_mapper
+    from_index: usize,
+    lexed_grammars: &mut HashSet<usize>,
+    lexed_without_expectation: bool,
+    global: &Rc<RefCell<Global>>,
+  ) -> Option<
+    StateTryLexOutput<
+      TNode<TKind, NTKind, ASTData, ErrorType, Global>,
+      TrimmedLexer<'buffer, TKind, LexerActionState, LexerErrorType>,
+    >,
+  > {
+    for (i, gr) in self.candidates[from_index..].iter().enumerate() {
+      if let Some(output) = gr.try_lex(
+        self.digested,
+        lexer,
+        lexed_grammars,
+        lexed_without_expectation,
+        global,
+      ) {
+        return Some(StateTryLexOutput {
+          node: output.node,
+          lexer: output.lexer,
+          next_candidate_index: i + 1,
+        });
+      }
+    }
+    // no candidate matches
+    None
+  }
+}
+
+pub struct StateTryLexOutput<NodeType, LexerType> {
+  pub node: NodeType,
+  pub lexer: LexerType,
+  pub next_candidate_index: usize,
 }
