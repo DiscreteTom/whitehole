76d1a473fd95c9e3fe083f581c084ab2441d30bc
DiscreteTom <discrete_tom@outlook.com>
2024-02-09 22:59:15 +0800

    chore(parser): add GrammarRuleRepo.grs

--- src/parser/elr/grammar/grammar_rule_repo.rs
+++ src/parser/elr/grammar/grammar_rule_repo.rs
@@ -1,5 +1,6 @@
 use super::grammar_rule::GrammarRule;
 use crate::lexer::token::TokenKind;
+use std::rc::Rc;
 
 pub struct GrammarRuleRepo<
   TKind: TokenKind,
@@ -8,7 +9,7 @@ pub struct GrammarRuleRepo<
   ErrorType: 'static,
   Global: 'static,
 > {
-  grs: Vec<GrammarRule<TKind, NTKind, ASTData, ErrorType, Global>>,
+  grs: Vec<Rc<GrammarRule<TKind, NTKind, ASTData, ErrorType, Global>>>,
 }
 
 impl<
@@ -19,7 +20,11 @@ impl<
     Global: 'static,
   > GrammarRuleRepo<TKind, NTKind, ASTData, ErrorType, Global>
 {
-  pub fn new(grs: Vec<GrammarRule<TKind, NTKind, ASTData, ErrorType, Global>>) -> Self {
+  pub fn new(grs: Vec<Rc<GrammarRule<TKind, NTKind, ASTData, ErrorType, Global>>>) -> Self {
     Self { grs }
+  }
+
+  pub fn grs(&self) -> &Vec<Rc<GrammarRule<TKind, NTKind, ASTData, ErrorType, Global>>> {
+    &self.grs
   }
 }
