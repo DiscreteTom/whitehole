4280e5f4518a211e0dc8b0f6809ce2f01b11e53f
DiscreteTom <discrete_tom@outlook.com>
2024-02-10 16:57:21 +0800

    chore(parser): optimize code

--- src/parser/elr/dfa/generate/builder.rs
+++ src/parser/elr/dfa/generate/builder.rs
@@ -24,6 +24,11 @@ pub fn prepare<
   let cs = CandidateRepo::with_initial(gr_repo.grs());
 }
 
+/// If a rule starts with an NT, merge result with that NT's grammar rules.
+///
+/// E.g. knowing `A := B 'c'` and `B := 'd'`, we can infer `A := 'd' 'c'`.
+/// When we construct DFA state, if a state has the candidate `A := # B 'c'`,
+/// it should also have the candidate `B := # 'd'`.
 fn calc_gr_closure<
   TKind: TokenKind,
   NTKind: TokenKind + Clone,
@@ -34,46 +39,55 @@ fn calc_gr_closure<
   grs: Vec<Rc<GrammarRule<TKind, NTKind, ASTData, ErrorType, Global>>>,
   gr_repo: GrammarRuleRepo<TKind, NTKind, ASTData, ErrorType, Global>,
 ) -> Vec<Rc<GrammarRule<TKind, NTKind, ASTData, ErrorType, Global>>> {
-  let mut result = grs;
-  let mut appended_gr_ids = result
-    .iter()
-    .map(|gr| gr.id().clone())
-    .collect::<HashSet<_>>();
-  let mut done_grs = HashSet::new();
+  // init result as a hashmap with given grammar rules
+  // so we can check if a grammar rule is already in the result by id
+  let mut result = grs
+    .into_iter()
+    .map(|gr| (gr.id().clone(), gr))
+    .collect::<HashMap<_, _>>();
+
+  // at the beginning, treat all given grammar rules as unexpanded
+  // we use hashmap to we can just swap this and next_unexpanded later
+  let mut unexpanded = result.clone();
 
   loop {
-    let mut to_be_appended = HashMap::new();
+    // keep track of next unexpanded grammar rules
+    // we use hashmap to check if a grammar rule is already in this by id
+    // and store the grammar rule as value
+    let mut next_unexpanded = HashMap::new();
 
-    result.iter().for_each(|gr| {
+    unexpanded.iter().for_each(|(_, gr)| {
+      // only expand if the unexpanded grammar rule's first grammar is NT
       if let GrammarKind::NT(nt) = gr.rule()[0].kind() {
-        if done_grs.contains(gr.id()) {
-          return;
-        }
-
-        // mark this gr as done
-        done_grs.insert(gr.id().clone());
-
         gr_repo
           .grs()
           .iter()
+          // find all grammar rules that yields the NT
           .filter(|gr2| gr2.nt().id() == nt.id())
           .for_each(|gr2| {
-            if !appended_gr_ids.contains(gr2.id()) && !to_be_appended.contains_key(gr2.id()) {
-              to_be_appended.insert(gr2.id(), gr2.clone());
+            // no need to check if the gr2 is already in `unexpanded`
+            // since unexpanded grammar rules are already in the result
+            // so we just make sure the gr2 is not already in the result and next_unexpanded
+            if !result.contains_key(gr2.id()) && !next_unexpanded.contains_key(gr2.id()) {
+              next_unexpanded.insert(gr2.id().clone(), gr2.clone());
             }
           });
       }
     });
 
-    if to_be_appended.len() == 0 {
+    if next_unexpanded.len() == 0 {
+      // done
       break;
     }
 
-    for (id, gr) in to_be_appended {
-      appended_gr_ids.insert(id.clone());
-      result.push(gr);
+    // append next_unexpanded to result
+    for (id, gr) in &next_unexpanded {
+      result.insert(id.clone(), gr.clone());
     }
+    // and swap unexpanded with next_unexpanded
+    unexpanded = next_unexpanded;
   }
 
-  result
+  // convert back to Vec
+  result.into_iter().map(|(_, gr)| gr).collect()
 }
