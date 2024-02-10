bd02e275e048dd471ba6ff46836b78f950e617d9
DiscreteTom <discrete_tom@outlook.com>
2024-02-10 17:56:55 +0800

    chore(parser): add StateRepo.calc_all_states

--- src/parser/elr/dfa/generate/state_repo.rs
+++ src/parser/elr/dfa/generate/state_repo.rs
@@ -17,7 +17,7 @@ use std::{
 pub struct StateRepo {
   states: HashMap<StateId, RawState>,
   // BTreeSet will store elements in a sorted order and can be hashed
-  cache: HashMap<BTreeSet<CandidateId>, StateId>,
+  cache: HashSet<BTreeSet<CandidateId>>,
 }
 
 impl StateRepo {
@@ -28,11 +28,15 @@ impl StateRepo {
     states.insert(state_id, entry_state);
     Self {
       states,
-      cache: HashMap::new(),
+      cache: HashSet::new(),
     }
   }
 
-  pub fn get_or_add_next<
+  pub fn states(&self) -> &HashMap<StateId, RawState> {
+    &self.states
+  }
+
+  pub fn calc_all_states<
     TKind: TokenKind,
     NTKind: TokenKind + Clone,
     ASTData: 'static,
@@ -40,7 +44,66 @@ impl StateRepo {
     Global: 'static,
   >(
     &mut self,
-    current: &RawState,
+    input_grammar_ids: &HashSet<GrammarId>,
+    cs: &mut CandidateRepo<TKind, NTKind, ASTData, ErrorType, Global>,
+    // TODO: nt_closures only store grammar rule id?
+    nt_closures: &HashMap<
+      TokenKindId,
+      Vec<Rc<GrammarRule<TKind, NTKind, ASTData, ErrorType, Global>>>,
+    >,
+  ) {
+    // TODO: optimize code
+
+    // store the candidates of each unexpanded state
+    let mut unexpanded = self
+      .states
+      .iter()
+      // TODO: prevent the clone
+      .map(|(_, state)| state.candidates().clone())
+      .collect::<Vec<_>>();
+    loop {
+      let mut generated = Vec::new();
+      input_grammar_ids.iter().for_each(|input_grammar_id| {
+        unexpanded.iter().for_each(|current_candidates| {
+          if let Some(next_candidates) =
+            self.generate_next(current_candidates, input_grammar_id, cs, nt_closures)
+          {
+            generated.push(next_candidates)
+          }
+        });
+      });
+
+      // TODO: don't convert?
+      unexpanded = generated
+        .iter()
+        .map(|c| c.iter().map(|c| c.clone()).collect())
+        .collect::<Vec<_>>();
+
+      generated.into_iter().for_each(|next_candidates| {
+        let id = self.states.len();
+        let state = RawState::new(
+          id,
+          next_candidates
+            .iter()
+            .map(|c| c.clone())
+            .collect::<Vec<_>>(),
+        );
+        self.states.insert(id, state);
+        self.cache.insert(next_candidates);
+      });
+    }
+  }
+
+  /// Return `None` if no next or next is already generated.
+  fn generate_next<
+    TKind: TokenKind,
+    NTKind: TokenKind + Clone,
+    ASTData: 'static,
+    ErrorType: 'static,
+    Global: 'static,
+  >(
+    &self,
+    current_candidates: &Vec<CandidateId>,
     input_grammar_id: &GrammarId,
     cs: &mut CandidateRepo<TKind, NTKind, ASTData, ErrorType, Global>,
     // TODO: nt_closures only store grammar rule id?
@@ -48,8 +111,9 @@ impl StateRepo {
       TokenKindId,
       Vec<Rc<GrammarRule<TKind, NTKind, ASTData, ErrorType, Global>>>,
     >,
-  ) -> Option<StateId> {
-    let next_candidates = Self::get_next_candidates(current, input_grammar_id, cs, nt_closures);
+  ) -> Option<BTreeSet<CandidateId>> {
+    let next_candidates =
+      Self::calc_next_candidates(current_candidates, input_grammar_id, cs, nt_closures);
 
     if next_candidates.len() == 0 {
       // no next state
@@ -57,33 +121,23 @@ impl StateRepo {
     }
 
     // check cache whether the state already created
-    // TODO: check another cache by current and input grammar id?
-    if let Some(cache) = self.cache.get(&next_candidates) {
-      return Some(cache.clone());
+    if self.cache.contains(&next_candidates) {
+      return None;
     }
 
     // create new
-    let id = self.states.len();
-    let state = RawState::new(
-      id,
-      next_candidates
-        .iter()
-        .map(|c| c.clone())
-        .collect::<Vec<_>>(),
-    );
-    self.states.insert(id, state);
-    self.cache.insert(next_candidates, id);
-    Some(id)
+    Some(next_candidates)
   }
 
-  fn get_next_candidates<
+  // TODO: merge with generate_next
+  fn calc_next_candidates<
     TKind: TokenKind,
     NTKind: TokenKind + Clone,
     ASTData: 'static,
     ErrorType: 'static,
     Global: 'static,
   >(
-    current: &RawState,
+    current_candidates: &Vec<CandidateId>,
     input_grammar_id: &GrammarId,
     cs: &mut CandidateRepo<TKind, NTKind, ASTData, ErrorType, Global>,
     // TODO: nt_closures only store grammar rule id?
@@ -95,8 +149,7 @@ impl StateRepo {
     // TODO: optimize code
     let mut nts = HashSet::new();
     // find grammar rules that can accept the input grammar
-    let mut next_candidates = current
-      .candidates()
+    let mut next_candidates = current_candidates
       .iter()
       .map(|c_id| {
         cs.get_or_add_next(c_id, input_grammar_id).map(|next| {
