1f835f5c14e75ac1d67f70f74428a5d09ff12da1
DiscreteTom <discrete_tom@outlook.com>
2024-02-12 10:53:30 +0800

    chore(parser): add GrammarRuleRepo.push

--- src/parser/elr/dfa/generate/grammar_rule_repo.rs
+++ src/parser/elr/dfa/generate/grammar_rule_repo.rs
@@ -1,5 +1,14 @@
-use crate::{lexer::token::TokenKind, parser::elr::grammar::grammar_rule::GrammarRule};
-use std::rc::Rc;
+use crate::{
+  lexer::token::TokenKind,
+  parser::{
+    elr::grammar::{
+      grammar::Grammar,
+      grammar_rule::{GrammarRule, GrammarRuleId},
+    },
+    traverser::Traverser,
+  },
+};
+use std::{collections::HashSet, rc::Rc};
 
 pub struct GrammarRuleRepo<
   TKind: TokenKind<TKind>,
@@ -17,13 +26,35 @@ impl<
     ASTData: 'static,
     ErrorType: 'static,
     Global: 'static,
+  > Default for GrammarRuleRepo<TKind, NTKind, ASTData, ErrorType, Global>
+{
+  fn default() -> Self {
+    Self { grs: Vec::new() }
+  }
+}
+
+impl<
+    TKind: TokenKind<TKind>,
+    NTKind: TokenKind<NTKind> + Clone,
+    ASTData: 'static,
+    ErrorType: 'static,
+    Global: 'static,
   > GrammarRuleRepo<TKind, NTKind, ASTData, ErrorType, Global>
 {
-  pub fn new(grs: Vec<Rc<GrammarRule<TKind, NTKind, ASTData, ErrorType, Global>>>) -> Self {
-    Self { grs }
-  }
-
   pub fn grs(&self) -> &Vec<Rc<GrammarRule<TKind, NTKind, ASTData, ErrorType, Global>>> {
     &self.grs
+  }
+
+  pub fn push(
+    &mut self,
+    nt: Rc<Grammar<TKind, NTKind>>,
+    rule: Vec<Rc<Grammar<TKind, NTKind>>>,
+    expect: HashSet<usize>,
+    traverser: Traverser<TKind, NTKind, ASTData, ErrorType, Global>,
+  ) {
+    let id = GrammarRuleId(self.grs.len());
+    self
+      .grs
+      .push(Rc::new(GrammarRule::new(id, nt, rule, expect, traverser)));
   }
 }
