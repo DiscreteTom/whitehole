ae4c8456f46c2da70f2c506dd00bc2c3aad04bf5
DiscreteTom <discrete_tom@outlook.com>
2024-03-02 22:51:24 +0800

    tests(lexer): add tests for action::select

--- src/lexer/action/select.rs
+++ src/lexer/action/select.rs
@@ -9,24 +9,24 @@ use std::collections::HashSet;
 
 impl<Kind, ActionState, ErrorType> Action<Kind, ActionState, ErrorType> {
   /// Set [`Action::possible_kinds`].
-  /// This is used to accelerate the lexing process when lexing with expected kinds.
-  pub fn kinds<NewKind: 'static>(
-    self,
-    possible_kinds: impl Into<Vec<NewKind>>,
-  ) -> MultiKindAction<NewKind, Kind, ActionState, ErrorType>
-  where
-    NewKind: TokenKind<NewKind>,
-  {
-    MultiKindAction {
-      possible_kinds: possible_kinds.into().iter().map(|kind| kind.id()).collect(),
-      head_matcher: self.head_matcher,
-      maybe_muted: self.maybe_muted,
-      exec: self.exec,
-    }
-  }
-
-  /// Set [`Action::possible_kinds`].
-  /// This is used to accelerate the lexing process when lexing with expected kinds.
+  /// # Examples
+  /// ```
+  /// # use whitehole::lexer::action::regex::regex;
+  /// # use whitehole::lexer::action::Action;
+  /// # use whitehole::lexer::token::TokenKind;
+  /// # use whitehole_macros::TokenKind;
+  /// # use Num::*;
+  /// # #[derive(TokenKind, Clone)]
+  /// # enum Num { Even, Odd }
+  /// # let action: Action<Num, (), ()> =
+  /// regex(r"^\d+").unwrap().kind_ids([Even.id(), Odd.id()]).select(|ctx| {
+  ///   if ctx.output.content().parse::<u32>().unwrap() % 2 == 0 {
+  ///     Even
+  ///   } else {
+  ///     Odd
+  ///   }
+  /// });
+  /// ```
   pub fn kind_ids<NewKind: 'static>(
     self,
     possible_kinds: impl Into<HashSet<TokenKindId<NewKind>>>,
@@ -37,6 +37,40 @@ impl<Kind, ActionState, ErrorType> Action<Kind, ActionState, ErrorType> {
       maybe_muted: self.maybe_muted,
       exec: self.exec,
     }
+  }
+
+  /// Set [`Action::possible_kinds`].
+  /// # Examples
+  /// ```
+  /// # use whitehole::lexer::action::regex::regex;
+  /// # use whitehole::lexer::action::Action;
+  /// # use whitehole_macros::TokenKind;
+  /// # use Num::*;
+  /// # #[derive(TokenKind, Clone)]
+  /// # enum Num { Even, Odd }
+  /// # let action: Action<Num, (), ()> =
+  /// regex(r"^\d+").unwrap().kinds([Even, Odd]).select(|ctx| {
+  ///   if ctx.output.content().parse::<u32>().unwrap() % 2 == 0 {
+  ///     Even
+  ///   } else {
+  ///     Odd
+  ///   }
+  /// });
+  /// ```
+  pub fn kinds<NewKind: 'static>(
+    self,
+    possible_kinds: impl Into<Vec<NewKind>>,
+  ) -> MultiKindAction<NewKind, Kind, ActionState, ErrorType>
+  where
+    NewKind: TokenKind<NewKind>,
+  {
+    self.kind_ids(
+      possible_kinds
+        .into()
+        .into_iter()
+        .map(TokenKindId::from)
+        .collect::<HashSet<_>>(),
+    )
   }
 }
 
@@ -51,6 +85,25 @@ impl<NewKind, Kind: 'static, ActionState: 'static, ErrorType: 'static>
   MultiKindAction<NewKind, Kind, ActionState, ErrorType>
 {
   /// Define a selector to select a kind from action's kinds by action's input and output.
+  /// **Be ware**: the result won't be checked against `possible_kinds`
+  /// so make sure the result is in `possible_kinds`.
+  /// # Examples
+  /// ```
+  /// # use whitehole::lexer::action::regex::regex;
+  /// # use whitehole::lexer::action::Action;
+  /// # use whitehole_macros::TokenKind;
+  /// # use Num::*;
+  /// # #[derive(TokenKind, Clone)]
+  /// # enum Num { Even, Odd }
+  /// # let action: Action<Num, (), ()> =
+  /// regex(r"^\d+").unwrap().kinds([Even, Odd]).select(|ctx| {
+  ///   if ctx.output.content().parse::<u32>().unwrap() % 2 == 0 {
+  ///     Even
+  ///   } else {
+  ///     Odd
+  ///   }
+  /// });
+  /// ```
   pub fn select<F>(self, selector: F) -> Action<NewKind, ActionState, ErrorType>
   where
     F: Fn(&AcceptedActionDecoratorContext<Kind, ActionState, ErrorType>) -> NewKind + 'static,
@@ -75,5 +128,87 @@ impl<NewKind, Kind: 'static, ActionState: 'static, ErrorType: 'static>
       possible_kinds: self.possible_kinds,
       head_matcher: self.head_matcher,
     }
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use crate::lexer::action::regex::regex;
+  use whitehole_macros::_TokenKind;
+  use Num::*;
+
+  #[derive(_TokenKind, Clone)]
+  enum Num {
+    Even,
+    Odd,
+  }
+
+  fn test_runner<F>(f: F)
+  where
+    F: Fn(Action<()>) -> Action<Num>,
+  {
+    let action = f(regex(r"^\d+").unwrap().head_unknown().mute(true));
+
+    // ensure the possible kinds is set
+    assert_eq!(action.possible_kinds.len(), 2);
+    assert!(action.possible_kinds.contains(&Even.id()));
+    assert!(action.possible_kinds.contains(&Odd.id()));
+
+    // ensure other fields are not changed
+    assert!(action.maybe_muted);
+    assert!(matches!(
+      action.head_matcher,
+      Some(ActionInputRestHeadMatcher::Unknown)
+    ));
+
+    // ensure the result is correct
+    let mut state = ();
+    let mut input = ActionInput::new("123", 0, &mut state);
+    assert!(matches!(
+      action.exec(&mut input),
+      Some(ActionOutput {
+        kind: Odd,
+        digested: 3,
+        muted: true,
+        error: None
+      })
+    ));
+    let mut input = ActionInput::new("124", 0, &mut state);
+    assert!(matches!(
+      action.exec(&mut input),
+      Some(ActionOutput {
+        kind: Even,
+        digested: 3,
+        muted: true,
+        error: None
+      })
+    ));
+  }
+
+  #[test]
+  fn action_kind_ids() {
+    test_runner(|a| {
+      a.kind_ids([Even.id(), Odd.id()]).select(|ctx| {
+        if ctx.output.content().parse::<u32>().unwrap() % 2 == 0 {
+          Even
+        } else {
+          Odd
+        }
+      })
+    });
+  }
+
+  #[test]
+  fn action_kinds() {
+    test_runner(|a| {
+      a.kinds([Even, Odd]).select(|ctx| {
+        if ctx.output.content().parse::<u32>().unwrap() % 2 == 0 {
+          Even
+        } else {
+          Odd
+        }
+      })
+    });
   }
 }
