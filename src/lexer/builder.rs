1bad12fee1103bf9b1cf6049f76c7a183d15da7f
DiscreteTom <discrete_tom@outlook.com>
2024-03-03 23:00:18 +0800

    chore(lexer): move the building process of StatelessLexer into LexerBuilder

--- src/lexer/builder.rs
+++ src/lexer/builder.rs
@@ -5,8 +5,13 @@ mod ignore;
 
 pub use action_list::*;
 
-use super::{action::Action, stateless::StatelessLexer, token::TokenKind, Lexer};
-use std::rc::Rc;
+use super::{
+  action::{Action, ActionInputRestHeadMatcher},
+  stateless::{ActionHeadMap, StatelessLexer},
+  token::TokenKind,
+  Lexer,
+};
+use std::{collections::HashMap, rc::Rc};
 
 pub struct LexerBuilder<Kind, ActionState = (), ErrorType = ()> {
   actions: Vec<Action<Kind, ActionState, ErrorType>>,
@@ -53,18 +58,48 @@ impl<Kind, ActionState, ErrorType> LexerBuilder<Kind, ActionState, ErrorType> {
     Self::default()
   }
 
-  fn map_actions<OldKind: 'static, NewKind, F>(
-    actions: impl Into<ActionList<Action<OldKind, ActionState, ErrorType>>>,
-    f: F,
-  ) -> Vec<Action<NewKind, ActionState, ErrorType>>
-  where
-    F: Fn(Action<OldKind, ActionState, ErrorType>) -> Action<NewKind, ActionState, ErrorType>,
-  {
-    actions.into().0.into_iter().map(f).collect::<Vec<_>>()
-  }
-
+  // TODO: move into `generate`?
   pub fn build_stateless(self) -> StatelessLexer<Kind, ActionState, ErrorType> {
-    StatelessLexer::new(self.actions)
+    // wrap actions in Rc and make them immutable
+    let actions = self.actions.into_iter().map(Rc::new).collect::<Vec<_>>();
+
+    let mut kind_map = HashMap::new();
+    // prepare kind map, add value for all possible kinds
+    for a in &actions {
+      for k in a.possible_kinds() {
+        kind_map.entry(k.clone()).or_insert(Vec::new());
+      }
+    }
+    // fill kind_map
+    for a in &actions {
+      if a.maybe_muted {
+        // maybe muted, add to all kinds
+        for (_, vec) in kind_map.iter_mut() {
+          vec.push(a.clone());
+        }
+      } else {
+        // never muted, only add to possible kinds
+        for k in a.possible_kinds() {
+          kind_map.get_mut(k).unwrap().push(a.clone());
+        }
+      }
+    }
+    // the above code should make sure the order of actions in each vec is the same as the order in `actions`
+
+    let maybe_muted_actions = actions
+      .iter()
+      .filter(|a| a.maybe_muted)
+      .map(|a| a.clone())
+      .collect();
+
+    let kind_head_map = kind_map
+      .iter()
+      .map(|(k, v)| (k.clone(), Self::calc_head_map(&v)))
+      .collect();
+    let head_map = Self::calc_head_map(&actions);
+    let maybe_muted_head_map = Self::calc_head_map(&maybe_muted_actions);
+
+    StatelessLexer::new(actions, head_map, kind_head_map, maybe_muted_head_map)
   }
 
   pub fn build_with<'text>(
@@ -80,6 +115,70 @@ impl<Kind, ActionState, ErrorType> LexerBuilder<Kind, ActionState, ErrorType> {
     ActionState: Default,
   {
     self.build_with(ActionState::default(), text)
+  }
+
+  fn calc_head_map(
+    actions: &Vec<Rc<Action<Kind, ActionState, ErrorType>>>,
+  ) -> ActionHeadMap<Kind, ActionState, ErrorType> {
+    let mut head_map = ActionHeadMap {
+      known_map: HashMap::new(),
+      unknown_fallback: Vec::new(),
+    };
+    // collect all known chars
+    for a in actions {
+      if let Some(head_matcher) = a.head_matcher() {
+        for c in match head_matcher {
+          ActionInputRestHeadMatcher::OneOf(set) => set,
+          ActionInputRestHeadMatcher::Not(set) => set,
+          ActionInputRestHeadMatcher::Unknown => continue,
+        } {
+          head_map.known_map.entry(*c).or_insert(Vec::new());
+        }
+      }
+    }
+    // fill the head_map
+    for a in actions {
+      if let Some(head_matcher) = a.head_matcher() {
+        match head_matcher {
+          ActionInputRestHeadMatcher::OneOf(set) => {
+            for c in set {
+              head_map.known_map.get_mut(c).unwrap().push(a.clone());
+            }
+          }
+          ActionInputRestHeadMatcher::Not(set) => {
+            for (c, vec) in head_map.known_map.iter_mut() {
+              if !set.contains(c) {
+                vec.push(a.clone());
+              }
+            }
+            head_map.unknown_fallback.push(a.clone());
+          }
+          ActionInputRestHeadMatcher::Unknown => {
+            head_map.unknown_fallback.push(a.clone());
+          }
+        }
+      } else {
+        // no head matcher, add to all known chars
+        for vec in head_map.known_map.values_mut() {
+          vec.push(a.clone());
+        }
+        // and unknown fallback
+        head_map.unknown_fallback.push(a.clone());
+      }
+    }
+    // the above code should make sure the order of actions in each vec is the same as the order in `actions`
+
+    head_map
+  }
+
+  fn map_actions<OldKind: 'static, NewKind, F>(
+    actions: impl Into<ActionList<Action<OldKind, ActionState, ErrorType>>>,
+    f: F,
+  ) -> Vec<Action<NewKind, ActionState, ErrorType>>
+  where
+    F: Fn(Action<OldKind, ActionState, ErrorType>) -> Action<NewKind, ActionState, ErrorType>,
+  {
+    actions.into().0.into_iter().map(f).collect::<Vec<_>>()
   }
 }
 
