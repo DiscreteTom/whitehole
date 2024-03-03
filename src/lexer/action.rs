62d2b75a3940b59059f32f6ff1544f60f7dda33e
DiscreteTom <discrete_tom@outlook.com>
2024-03-03 10:50:00 +0800

    chore(lexer): update comments

--- src/lexer/action.rs
+++ src/lexer/action.rs
@@ -21,7 +21,7 @@ use std::collections::HashSet;
 pub enum ActionInputRestHeadMatcher {
   OneOf(HashSet<char>),
   Not(HashSet<char>),
-  /// Match any characters that are not in known `OneOf` or `Not`.
+  /// Match any characters that are not known in `OneOf` or `Not`.
   Unknown,
 }
 
@@ -30,20 +30,21 @@ pub struct Action<Kind: 'static, ActionState: 'static = (), ErrorType: 'static =
   /// The lexer will based on this flag to accelerate the lexing process.
   /// If `true`, this action's output may be muted.
   /// If `false`, this action's output will never be muted.
-  /// For most cases this field will be set automatically,
-  /// so don't set this field unless you know what you are doing.
+  /// For most cases this field will be set automatically (e.g. via [`Action::mute`] or [`Action::mute_if`]),
+  /// so don't set this field manually unless you know what you are doing.
   pub maybe_muted: bool,
 
-  /// This is used to accelerate expectational lexing.
-  /// Every action should have this field set by [`Action::kinds`].
+  /// See [`Action::possible_kinds`].
   possible_kinds: HashSet<TokenKindId<Kind>>,
-  /// This is used to accelerate lexing by the first character
-  /// of the rest of the input. This is optional.
+  /// See [`Action::head_matcher`].
   head_matcher: Option<ActionInputRestHeadMatcher>,
+  // input is mutable so the action can mutate the action state.
   exec: Box<dyn Fn(&mut ActionInput<ActionState>) -> Option<ActionOutput<Kind, ErrorType>>>,
 }
 
 impl<ActionState, ErrorType> Action<(), ActionState, ErrorType> {
+  /// Create a new action with no kind.
+  /// To set the kind, use [`Action::bind`], [`Action::kinds`] or [`Action::kind_ids`].
   pub fn new<F>(exec: F) -> Self
   where
     F: Fn(&mut ActionInput<ActionState>) -> Option<ActionOutputWithoutKind<ErrorType>> + 'static,
@@ -63,11 +64,14 @@ impl<Kind, ActionState, ErrorType> Action<Kind, ActionState, ErrorType> {
     !self.maybe_muted
   }
 
-  /// Should only be set by [`Action::bind`], [`Action::kinds`] or [`Action::kind_ids`].
+  /// This is used to accelerate expectational lexing.
+  /// Every action should have this field set by [`Action::bind`] [`Action::kinds`] or [`Action::kind_ids`].
   pub fn possible_kinds(&self) -> &HashSet<TokenKindId<Kind>> {
     &self.possible_kinds
   }
 
+  /// This is used to accelerate lexing by the first character
+  /// of the rest of the input. This is optional but highly recommended.
   /// Should only be set by [`Action::head_in`], [`Action::head_not`] and [`Action::head_unknown`].
   pub fn head_matcher(&self) -> &Option<ActionInputRestHeadMatcher> {
     &self.head_matcher
