adddf54af11e6502b5f70f38c32648d9eefa825b
DiscreteTom <discrete_tom@outlook.com>
2024-03-04 08:59:17 +0800

    tests(lexer): add unit tests for whitespaces

--- src/lexer/action/utils.rs
+++ src/lexer/action/utils.rs
@@ -96,3 +96,44 @@ pub fn word<ActionState: 'static, ErrorType: 'static>(
       .unwrap_or(false)
   })
 }
+
+// TODO: add tests
+// TODO: add string & numeric utils
+
+#[cfg(test)]
+mod tests {
+  use crate::lexer::action::ActionInput;
+
+  use super::*;
+  #[test]
+  fn action_utils_whitespaces() {
+    let action: Action<()> = whitespaces();
+
+    // common cases
+    let text = " \n\t";
+    assert_eq!(
+      action
+        .exec(&mut ActionInput::new(text, 0, &mut ()))
+        .unwrap()
+        .digested,
+      text.len()
+    );
+
+    // full cases
+    let text: String = [
+      '\u{0009}', '\u{000A}', '\u{000B}', '\u{000C}', '\u{000D}', '\u{0020}', '\u{0085}',
+      '\u{00A0}', '\u{1680}', '\u{2000}', '\u{2001}', '\u{2002}', '\u{2003}', '\u{2004}',
+      '\u{2005}', '\u{2006}', '\u{2007}', '\u{2008}', '\u{2009}', '\u{200A}', '\u{2028}',
+      '\u{2029}', '\u{202F}', '\u{205F}', '\u{3000}',
+    ]
+    .into_iter()
+    .collect();
+    assert_eq!(
+      action
+        .exec(&mut ActionInput::new(text.as_str(), 0, &mut ()))
+        .unwrap()
+        .digested,
+      text.len()
+    );
+  }
+}
