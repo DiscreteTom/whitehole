3b14600ecd78de5a5b1e873ae8b5df1ab1fbfb67
DiscreteTom <discrete_tom@outlook.com>
2024-03-04 21:30:33 +0800

    chore(lexer): move exact & word family into single file

---
+++ src/lexer/action/utils/exact.rs
@@ -0,0 +1,171 @@
+use super::StringList;
+use crate::lexer::{action::simple, Action};
+use std::collections::HashSet;
+
+/// Match one of the provided strings exactly, in one action, ***NO LOOKAHEAD***.
+/// Stop at the first match.
+/// The head matcher will be set automatically.
+/// # Examples
+/// ```
+/// # use whitehole::lexer::action::{Action, exact};
+/// # let action: Action<()> =
+/// // single string
+/// exact("a");
+/// # let action: Action<()> =
+/// // multiple strings
+/// // try to match "a" first, then "b", in one action
+/// exact(["a", "b"]);
+/// ```
+/// # Caveats
+/// Be ware if you provide multiple strings:
+/// ```
+/// # use whitehole::lexer::action::{Action, exact};
+/// // this will always match `"a"` and never match `"ab"`
+/// # let action: Action<()> =
+/// exact(["a", "ab"]);
+/// // this will skip the check of `"a"` when re-lex
+/// // since this is one action instead of two.
+/// # let action: Action<()> =
+/// exact(["ab", "a"]);
+/// ```
+/// To avoid the above, try [`exact_vec`] or [`exact_chars`].
+pub fn exact<ActionState, ErrorType>(
+  ss: impl Into<StringList>,
+) -> Action<(), ActionState, ErrorType> {
+  let ss: Vec<String> = ss.into().0;
+
+  if ss.len() == 0 {
+    panic!("empty string list");
+  }
+
+  // optimize for single string // TODO: is this needed?
+  if ss.len() == 1 {
+    let s = ss.into_iter().next().unwrap();
+    let head = s.chars().next().unwrap();
+    return simple(move |input| {
+      if input.rest().starts_with(&s) {
+        s.len()
+      } else {
+        0
+      }
+    })
+    .head_in([head]);
+  }
+
+  let heads: HashSet<_> = ss.iter().map(|s| s.chars().next().unwrap()).collect();
+  simple(move |input| {
+    for s in &ss {
+      if input.rest().starts_with(s) {
+        return s.len();
+      }
+    }
+    0 // no match
+  })
+  .head_in(heads)
+}
+
+/// Similar to [`exact`], but create an action for each string.
+/// # Examples
+/// ```
+/// # use whitehole::lexer::action::{Action, exact_vec};
+/// # let actions: Vec<Action<()>> =
+/// exact_vec(["++", "--"]);
+/// ```
+pub fn exact_vec<ActionState, ErrorType>(
+  ss: impl Into<StringList>,
+) -> Vec<Action<(), ActionState, ErrorType>> {
+  let ss: Vec<String> = ss.into().0;
+
+  if ss.len() == 0 {
+    panic!("empty string list");
+  }
+
+  ss.into_iter().map(|s| exact(s)).collect()
+}
+
+/// Similar to [`exact`], but accept one string
+/// and create an action for each char.
+/// # Examples
+/// ```
+/// # use whitehole::lexer::action::{Action, exact_vec};
+/// # let actions: Vec<Action<()>> =
+/// exact_chars("+-*/()");
+/// ```
+pub fn exact_chars<ActionState, ErrorType>(
+  s: impl Into<String>,
+) -> Vec<Action<(), ActionState, ErrorType>> {
+  let s: String = s.into();
+
+  if s.len() == 0 {
+    panic!("empty string");
+  }
+
+  s.chars().map(|c| exact(c.to_string())).collect()
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use crate::lexer::action::{ActionInput, ActionInputRestHeadMatcher};
+
+  #[test]
+  #[should_panic]
+  fn action_utils_exact_empty() {
+    exact::<(), ()>(vec![]);
+  }
+
+  #[test]
+  fn action_utils_exact() {
+    // single string
+    let action: Action<()> = exact("a");
+    let text = "a";
+    assert_eq!(
+      action
+        .exec(&mut ActionInput::new(text, 0, &mut ()))
+        .unwrap()
+        .digested,
+      text.len()
+    );
+    // head matcher
+    assert!(matches!(
+      action.head_matcher().as_ref().unwrap(),
+      ActionInputRestHeadMatcher::OneOf(set) if set.len() == 1 && set.contains(&'a')
+    ));
+    // no lookahead
+    let text = "ab";
+    assert_eq!(
+      action
+        .exec(&mut ActionInput::new(text, 0, &mut ()))
+        .unwrap()
+        .digested,
+      1
+    );
+
+    // multi strings
+    let action: Action<()> = exact(["a", "b"]);
+    let text = "b";
+    assert_eq!(
+      action
+        .exec(&mut ActionInput::new(text, 0, &mut ()))
+        .unwrap()
+        .digested,
+      text.len()
+    );
+    // head matcher
+    assert!(matches!(
+      action.head_matcher().as_ref().unwrap(),
+      ActionInputRestHeadMatcher::OneOf(set) if set.len() == 2 && set.contains(&'a') && set.contains(&'b')
+    ));
+
+    // caveats
+    let action: Action<()> = exact(["a", "ab"]);
+    let text = "ab";
+    assert_eq!(
+      action
+        .exec(&mut ActionInput::new(text, 0, &mut ()))
+        .unwrap()
+        .digested,
+      1
+    );
+  }
+}
---
+++ src/lexer/action/utils/word.rs
@@ -0,0 +1,242 @@
+use super::StringList;
+use crate::lexer::{action::simple, Action};
+use std::collections::HashSet;
+
+/// Match one of the provided words, in one action,
+/// ***LOOKAHEAD*** one char to ensure there is a word boundary
+/// (alphanumeric or `_`) or end of input after the word.
+/// Stop at the first match.
+/// The head matcher will be set automatically.
+/// # Examples
+/// ```
+/// # use whitehole::lexer::action::{Action, word};
+/// # let action: Action<()> =
+/// // single word
+/// word("a");
+/// # let action: Action<()> =
+/// // multiple words
+/// // try to match "a" first, then "b", in one action
+/// word(["a", "b"]);
+/// ```
+/// # Caveats
+/// Be ware if you provide multiple words:
+/// ```
+/// # use whitehole::lexer::action::{Action, word};
+/// // this will skip the check of `"a"` when re-lex
+/// // since this is one action instead of two.
+/// # let action: Action<()> =
+/// word(["ab", "a"]);
+/// ```
+/// To avoid the above, try [`word_vec`] or [`word_chars`].
+pub fn word<ActionState, ErrorType>(
+  ss: impl Into<StringList>,
+) -> Action<(), ActionState, ErrorType> {
+  // don't use `exact(ss).reject_if(...)` here
+  // e.g. `exact(["a", "ab"])` will accept "ab" as "a"
+  // then reject since no word boundary after "a"
+  // however "ab" is accepted by `word(["a", "ab"])`
+
+  let ss: Vec<String> = ss.into().0;
+
+  if ss.len() == 0 {
+    panic!("empty word list");
+  }
+
+  // optimize for single string // TODO: is this needed?
+  if ss.len() == 1 {
+    let s = ss.into_iter().next().unwrap();
+    let head = s.chars().next().unwrap();
+    return simple(move |input| {
+      if input.rest().starts_with(&s)
+        && input.rest()[s.len()..]
+          .chars()
+          .next()
+          // if next char exists, it can't be alphanumeric or `_`
+          .map(|c| !c.is_alphanumeric() && c != '_')
+          // if no next char (EOF), it's ok
+          .unwrap_or(true)
+      {
+        s.len()
+      } else {
+        0
+      }
+    })
+    .head_in([head]);
+  }
+
+  let heads: HashSet<_> = ss.iter().map(|s| s.chars().next().unwrap()).collect();
+  simple(move |input| {
+    for s in &ss {
+      if input.rest().starts_with(s)
+        && input.rest()[s.len()..]
+          .chars()
+          .next()
+          // if next char exists, it can't be alphanumeric or `_`
+          .map(|c| !c.is_alphanumeric() && c != '_')
+          // if no next char (EOF), it's ok
+          .unwrap_or(true)
+      {
+        return s.len();
+      }
+    }
+    0 // no match
+  })
+  .head_in(heads)
+}
+
+/// Similar to [`word`], but create an action for each string.
+/// # Examples
+/// ```
+/// # use whitehole::lexer::action::{Action, word_vec};
+/// # let actions: Vec<Action<()>> =
+/// word_vec(["++", "--"]);
+/// ```
+pub fn word_vec<ActionState, ErrorType>(
+  ss: impl Into<StringList>,
+) -> Vec<Action<(), ActionState, ErrorType>> {
+  let ss: Vec<String> = ss.into().0;
+
+  if ss.len() == 0 {
+    panic!("empty word list");
+  }
+
+  ss.into_iter().map(|s| word(s)).collect()
+}
+
+/// Similar to [`word`], but accept one string
+/// and create an action for each char.
+/// # Examples
+/// ```
+/// # use whitehole::lexer::action::{Action, word_chars};
+/// # let actions: Vec<Action<()>> =
+/// word_chars("+-*/()");
+/// ```
+pub fn word_chars<ActionState, ErrorType>(
+  s: impl Into<String>,
+) -> Vec<Action<(), ActionState, ErrorType>> {
+  let s: String = s.into();
+
+  if s.len() == 0 {
+    panic!("empty string");
+  }
+
+  s.chars().map(|c| word(c.to_string())).collect()
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use crate::lexer::action::{ActionInput, ActionInputRestHeadMatcher};
+  #[test]
+  #[should_panic]
+  fn action_utils_word_empty() {
+    word::<(), ()>(vec![]);
+  }
+
+  #[test]
+  fn action_utils_word() {
+    // single string
+    let action: Action<()> = word("a");
+    let text = "a";
+    assert_eq!(
+      action
+        .exec(&mut ActionInput::new(text, 0, &mut ()))
+        .unwrap()
+        .digested,
+      text.len()
+    );
+    // head matcher
+    assert!(matches!(
+      action.head_matcher().as_ref().unwrap(),
+      ActionInputRestHeadMatcher::OneOf(set) if set.len() == 1 && set.contains(&'a')
+    ));
+    // lookahead
+    assert_eq!(
+      action
+        .exec(&mut ActionInput::new("a ", 0, &mut ()))
+        .unwrap()
+        .digested,
+      1
+    );
+    assert_eq!(
+      action
+        .exec(&mut ActionInput::new("a\t", 0, &mut ()))
+        .unwrap()
+        .digested,
+      1
+    );
+    assert_eq!(
+      action
+        .exec(&mut ActionInput::new("a\n", 0, &mut ()))
+        .unwrap()
+        .digested,
+      1
+    );
+    assert_eq!(
+      action
+        .exec(&mut ActionInput::new("a+", 0, &mut ()))
+        .unwrap()
+        .digested,
+      1
+    );
+    assert_eq!(
+      action
+        .exec(&mut ActionInput::new("a,", 0, &mut ()))
+        .unwrap()
+        .digested,
+      1
+    );
+    assert_eq!(
+      action
+        .exec(&mut ActionInput::new("a=", 0, &mut ()))
+        .unwrap()
+        .digested,
+      1
+    );
+    assert_eq!(
+      action
+        // punctuation in other languages also count as word boundary
+        .exec(&mut ActionInput::new("a，", 0, &mut ()))
+        .unwrap()
+        .digested,
+      1
+    );
+    assert!(action
+      .exec(&mut ActionInput::new("ab", 0, &mut ()))
+      .is_none());
+    assert!(action
+      .exec(&mut ActionInput::new("a_", 0, &mut ()))
+      .is_none());
+    assert!(action
+      .exec(&mut ActionInput::new("a我", 0, &mut ()))
+      .is_none());
+
+    // multi strings
+    let action: Action<()> = word(["a", "b"]);
+    let text = "b";
+    assert_eq!(
+      action
+        .exec(&mut ActionInput::new(text, 0, &mut ()))
+        .unwrap()
+        .digested,
+      text.len()
+    );
+    // head matcher
+    assert!(matches!(
+      action.head_matcher().as_ref().unwrap(),
+      ActionInputRestHeadMatcher::OneOf(set) if set.len() == 2 && set.contains(&'a') && set.contains(&'b')
+    ));
+
+    // caveats
+    // this is not acceptable by `exact` but acceptable by `word`
+    let action: Action<()> = word(["a", "ab"]);
+    let text = "ab";
+    assert_eq!(
+      action
+        .exec(&mut ActionInput::new(text, 0, &mut ()))
+        .unwrap()
+        .digested,
+      2
+    );
+  }
+}
