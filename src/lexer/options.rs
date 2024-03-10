48c60ec396a22535c4d155ec38839884b072741d
DiscreteTom <discrete_tom@outlook.com>
2024-03-10 17:17:45 +0800

    chore(lexer): optimize code for LexOptions

--- src/lexer/options.rs
+++ src/lexer/options.rs
@@ -24,7 +24,7 @@ impl Default for ReLexContext {
   }
 }
 
-pub struct LexOptions<'expect_text, Kind: 'static> {
+pub struct LexOptions<'expect_text, Kind> {
   pub expectation: Expectation<'expect_text, Kind>,
   /// If `true`, the [`LexOutput::re_lex`](crate::lexer::output::LexOutput::re_lex) might be `Some`.
   pub fork: bool,
@@ -32,7 +32,7 @@ pub struct LexOptions<'expect_text, Kind: 'static> {
   pub re_lex: Option<ReLexContext>,
 }
 
-impl<'expect_text, Kind: 'static> Default for LexOptions<'expect_text, Kind> {
+impl<'expect_text, Kind> Default for LexOptions<'expect_text, Kind> {
   fn default() -> Self {
     Self {
       expectation: Expectation::default(),
@@ -42,29 +42,29 @@ impl<'expect_text, Kind: 'static> Default for LexOptions<'expect_text, Kind> {
   }
 }
 
-impl<'expect_text, Kind: 'static> From<Expectation<'expect_text, Kind>>
-  for LexOptions<'expect_text, Kind>
-{
+impl<'expect_text, Kind> From<Expectation<'expect_text, Kind>> for LexOptions<'expect_text, Kind> {
   fn from(expectation: Expectation<'expect_text, Kind>) -> Self {
     Self::default().expect(expectation)
   }
 }
 
-impl<'expect_text, Kind: 'static> From<ReLexContext> for LexOptions<'expect_text, Kind> {
+impl<'expect_text, Kind> From<ReLexContext> for LexOptions<'expect_text, Kind> {
   fn from(re_lex: ReLexContext) -> Self {
     Self::default().re_lex(re_lex)
   }
 }
 
-impl<'expect_text, Kind: 'static> LexOptions<'expect_text, Kind> {
+impl<'expect_text, Kind> LexOptions<'expect_text, Kind> {
   pub fn expect(mut self, expectation: impl Into<Expectation<'expect_text, Kind>>) -> Self {
     self.expectation = expectation.into();
     self
   }
+  /// If set, the [`LexOutput::re_lex`](crate::lexer::output::LexOutput::re_lex) might be `Some`.
   pub fn fork(mut self) -> Self {
     self.fork = true;
     self
   }
+  /// Provide this if the lex is a re-lex.
   pub fn re_lex(mut self, re_lex: ReLexContext) -> Self {
     self.re_lex = Some(re_lex);
     self
