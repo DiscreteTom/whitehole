fa9862ea4ebb3d3e52af195077deb71dddd3b1d5
DiscreteTom <discrete_tom@outlook.com>
2024-03-14 18:32:03 +0800

    refactor(lexer): add TokenKindIdBinding, make TokenKind.id return ref

--- src/lexer/token/token_kind.rs
+++ src/lexer/token/token_kind.rs
@@ -1,19 +1,13 @@
-use std::{collections::HashSet, hash::Hash, marker::PhantomData};
+use std::{collections::HashSet, hash::Hash, marker::PhantomData, ops::Deref};
 
 pub trait TokenKind<TokenKindType> {
-  fn id(&self) -> TokenKindId<TokenKindType>;
+  fn id(&self) -> &TokenKindId<TokenKindType>;
 }
 
-/// The unique id of a token kind.
-/// Usually we use enum variants as token kinds, and the id is the variant's index.
+/// The unique id of a token kind value.
+/// Usually we use enum variants as token kind values, and the id is the variant's index.
 #[derive(Debug)]
 pub struct TokenKindId<TokenKindType>(pub usize, PhantomData<TokenKindType>);
-
-impl<TokenKindType: TokenKind<TokenKindType>> From<TokenKindType> for TokenKindId<TokenKindType> {
-  fn from(kind: TokenKindType) -> Self {
-    kind.id()
-  }
-}
 
 impl<TokenKindType> TokenKindId<TokenKindType> {
   pub fn new(id: usize) -> Self {
@@ -39,27 +33,75 @@ impl<TokenKindType> Clone for TokenKindId<TokenKindType> {
 }
 impl<TokenKindType> Copy for TokenKindId<TokenKindType> {}
 
-/// A mock struct which implements [`TokenKind`].
+/// Bind the token kind value with an [`TokenKindId`].
+/// This is readonly to make sure the binding is not broken.
+pub struct TokenKindIdBinding<TokenKindType> {
+  id: TokenKindId<TokenKindType>,
+  value: TokenKindType,
+}
+
+// value is private and need to be accessed by `value()`
+// so for convenience we impl Deref
+impl<TokenKindType> Deref for TokenKindIdBinding<TokenKindType> {
+  type Target = TokenKindType;
+  fn deref(&self) -> &Self::Target {
+    &self.value
+  }
+}
+// don't impl DerefMut because we want this to be readonly
+
+impl<TokenKindType> TokenKindIdBinding<TokenKindType> {
+  pub fn new(id: usize, value: TokenKindType) -> Self {
+    Self {
+      value,
+      id: TokenKindId::new(id),
+    }
+  }
+
+  pub fn id(&self) -> &TokenKindId<TokenKindType> {
+    &self.id
+  }
+  pub fn value(&self) -> &TokenKindType {
+    &self.value
+  }
+
+  /// Consume self and take the value out.
+  pub fn take(self) -> TokenKindType {
+    self.value
+  }
+}
+
+/// A mock struct which implements [`TokenKind`]
+/// and only has one possible kind id.
 /// This is useful in action utils to pass data to downstream actions.
 pub struct MockTokenKind<T> {
+  id: TokenKindId<MockTokenKind<T>>,
   pub data: T,
 }
 
 impl<T> MockTokenKind<T> {
-  /// Return the only possible kind id for [`MockTokenKind`].
+  /// Return the only possible kind id.
   pub fn id() -> TokenKindId<MockTokenKind<T>> {
+    // TODO: make the id static to prevent creation?
     TokenKindId::new(0)
   }
 
-  /// Return a [`HashSet`] containing the only possible kind id for [`MockTokenKind`].
+  /// Return a [`HashSet`] containing the only possible kind id.
   pub fn possible_kinds() -> HashSet<TokenKindId<MockTokenKind<T>>> {
     HashSet::from([Self::id()])
+  }
+
+  pub fn new(data: T) -> Self {
+    Self {
+      id: Self::id(),
+      data,
+    }
   }
 }
 
 impl<T> TokenKind<MockTokenKind<T>> for MockTokenKind<T> {
-  fn id(&self) -> TokenKindId<MockTokenKind<T>> {
-    TokenKindId::new(0)
+  fn id(&self) -> &TokenKindId<MockTokenKind<T>> {
+    &self.id
   }
 }
 
