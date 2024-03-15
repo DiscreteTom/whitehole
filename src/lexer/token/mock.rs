5efc43c838fbfdf8eac5ee9ae8f9b3cecb0b5808
DiscreteTom <discrete_tom@outlook.com>
2024-03-15 23:47:27 +0800

    chore(lexer): adopt new TokenKind for MockTokenKind

--- src/lexer/token/mock.rs
+++ src/lexer/token/mock.rs
@@ -6,6 +6,9 @@ use std::collections::HashSet;
 /// This is useful in action utils to pass data to downstream actions.
 pub struct MockTokenKind<T> {
   id: TokenKindId<MockTokenKind<T>>,
+  // the struct only have one possible id,
+  // so even the data is mutable the binding is not broken
+  // so we make the data public
   pub data: T,
 }
 
@@ -14,11 +17,6 @@ impl<T> MockTokenKind<T> {
   pub fn id() -> TokenKindId<MockTokenKind<T>> {
     // TODO: make the id static to prevent creation?
     TokenKindId::new(0)
-  }
-
-  /// Return a [`HashSet`] containing the only possible kind id.
-  pub fn possible_kinds() -> HashSet<TokenKindId<MockTokenKind<T>>> {
-    HashSet::from([Self::id()])
   }
 
   pub fn new(data: T) -> Self {
@@ -30,7 +28,15 @@ impl<T> MockTokenKind<T> {
 }
 
 impl<T> TokenKind<MockTokenKind<T>> for MockTokenKind<T> {
-  fn id(&self) -> &TokenKindId<MockTokenKind<T>> {
+  type TargetType = Self;
+
+  /// Return the only possible kind id.
+  fn id(&self) -> &TokenKindId<Self> {
     &self.id
+  }
+
+  /// Return a [`HashSet`] containing the only possible kind id.
+  fn possible_kinds() -> HashSet<TokenKindId<Self::TargetType>> {
+    HashSet::from([Self::id()])
   }
 }
