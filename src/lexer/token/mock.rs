0cca524fc0fb1cf2b88c88ad652931b5d9ae431e
DiscreteTom <discrete_tom@outlook.com>
2024-03-16 00:54:50 +0800

    feat(lexer)!: make MockTokenKind::id static, remove the id field

--- src/lexer/token/mock.rs
+++ src/lexer/token/mock.rs
@@ -5,38 +5,35 @@ use std::collections::HashSet;
 /// and only has one possible kind id.
 /// This is useful in action utils to pass data to downstream actions.
 pub struct MockTokenKind<T> {
-  id: TokenKindId<MockTokenKind<T>>,
   // the struct only have one possible id,
   // so even the data is mutable the binding is not broken
   // so we make the data public
   pub data: T,
 }
 
+// make the only possible kind id a static const
+// so that we don't need to create it every time
+// and we don't need to store it in the struct
+/// The only possible kind id of [`MockTokenKind`].
+const MOCK_TOKEN_KIND_ID: TokenKindId<MockTokenKind<()>> = TokenKindId::new(0);
+
 impl<T> MockTokenKind<T> {
   /// Return the only possible kind id.
-  pub fn id() -> TokenKindId<MockTokenKind<T>> {
-    // TODO: make the id static to prevent creation?
-    TokenKindId::new(0)
-  }
-
-  pub fn new(data: T) -> Self {
-    Self {
-      id: Self::id(),
-      data,
-    }
+  pub fn id() -> &'static TokenKindId<Self> {
+    &MOCK_TOKEN_KIND_ID.cast()
   }
 }
 
-impl<T> TokenKind<MockTokenKind<T>> for MockTokenKind<T> {
+impl<T> TokenKind<Self> for MockTokenKind<T> {
   type TargetType = Self;
 
   /// Return the only possible kind id.
   fn id(&self) -> &TokenKindId<Self> {
-    &self.id
+    Self::id()
   }
 
   /// Return a [`HashSet`] containing the only possible kind id.
   fn possible_kinds() -> HashSet<TokenKindId<Self::TargetType>> {
-    HashSet::from([Self::id()])
+    HashSet::from([Self::id().clone()])
   }
 }
