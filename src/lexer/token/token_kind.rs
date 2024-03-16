7edf0894e9cd10e9948c8aa987702a8ff7054d66
DiscreteTom <discrete_tom@outlook.com>
2024-03-16 17:39:43 +0800

    chore(lexer): remove unused code

--- src/lexer/token/token_kind.rs
+++ src/lexer/token/token_kind.rs
@@ -22,24 +22,3 @@ pub trait TokenKind<TokenKindType> {
   /// Return a set containing all possible kind ids of this token kind.
   fn possible_kinds() -> HashSet<TokenKindId<TokenKindType>>;
 }
-
-#[cfg(test)]
-mod tests {
-  use super::*;
-  use whitehole_macros::_TokenKind;
-  use MyKind::*;
-
-  #[derive(_TokenKind)]
-  enum MyKind {
-    UnitField,
-    UnnamedField(i32),
-    NamedField { _a: i32 },
-  }
-
-  #[test]
-  fn token_kind_id() {
-    assert_eq!(UnitField.id().0, 0);
-    assert_eq!(UnnamedField(42).id().0, 1);
-    assert_eq!(NamedField { _a: 1 }.id().0, 2);
-  }
-}
