934c2bd96879b6d8f0fcc7801a09bed995ae28b1
DiscreteTom <discrete_tom@outlook.com>
2024-03-26 22:30:52 +0800

    chore(lexer): update comments for ActionInput

--- src/lexer/action/input.rs
+++ src/lexer/action/input.rs
@@ -1,14 +1,18 @@
 pub struct ActionInput<'text, 'action_state, ActionState> {
-  // we store the whole text instead of only storing the rest of text
-  // so that users can check chars before the start position if needed
-  text: &'text str,
-  start: usize,
-  // cache the rest of the text
-  // to prevent create the slice every time
-  // because `input.rest` is frequently used across all actions
-  rest: &'text str,
-  // user can mutate the action state
+  // users can mutate the action state directly, so it's public.
+  // with the action state, users can build stateful lexer,
+  // while actions remain stateless and clone-able.
   pub state: &'action_state mut ActionState,
+  // users could access the whole input text instead of only the rest of text,
+  // to check chars before the start position if needed
+  /// See [`Self::text`].
+  text: &'text str,
+  /// See [`Self::start`].
+  start: usize,
+  // cache the rest of the text to prevent create the slice every time
+  // because `input.rest` is frequently used across all actions
+  /// See [`Self::rest`].
+  rest: &'text str,
 }
 
 impl<'text, 'action_state, ActionState> ActionInput<'text, 'action_state, ActionState> {
