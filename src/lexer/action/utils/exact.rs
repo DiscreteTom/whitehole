ee9be5bd044b91aa566190a09d4cc57b450c7cf9
DiscreteTom <discrete_tom@outlook.com>
2024-03-30 11:56:29 +0800

    feat(lexer)!: add `exact_sub`. `exact` only accept one string, update unit tests

--- src/lexer/action/utils/exact.rs
+++ src/lexer/action/utils/exact.rs
@@ -1,123 +1,72 @@
 use super::StringList;
 use crate::lexer::{
-  action::{simple, Action},
+  action::{simple, Action, SubAction},
   token::MockTokenKind,
 };
-use std::collections::HashSet;
 
-/// Match one of the provided strings exactly, in one action, ***NO LOOKAHEAD***.
-/// Stop at the first match.
+/// Match one string exactly, ***NO LOOKAHEAD***.
+/// # Examples
+/// ```
+/// # use whitehole::lexer::action::{SubAction, exact_sub};
+/// # let action: SubAction<()> =
+/// exact_sub(";");
+/// ```
+pub fn exact_sub<ActionState>(s: impl Into<String>) -> SubAction<ActionState> {
+  let s = s.into();
+  simple(move |input| {
+    if input.rest().starts_with(&s) {
+      s.len()
+    } else {
+      0
+    }
+  })
+}
+
+/// Match one string exactly, ***NO LOOKAHEAD***.
 ///
 /// The [`Action::head_matcher`] will be set automatically.
 /// # Examples
 /// ```
 /// # use whitehole::lexer::action::{Action, exact};
-/// # let action: Action<MockTokenKind<()>> =
-/// // single string
-/// exact("a");
-/// # let action: Action<MockTokenKind<()>> =
-/// // multiple strings
-/// // try to match "a" first, then "b", in one action
-/// exact(["a", "b"]);
+/// # let action: Action<_> =
+/// exact(";");
 /// ```
-/// # Caveats
-/// Be ware if you provide multiple strings:
-/// ```
-/// # use whitehole::lexer::action::{Action, exact};
-/// // this will always match `"a"` and never match `"ab"`
-/// # let action: Action<MockTokenKind<()>> =
-/// exact(["a", "ab"]);
-/// // this will skip the check of `"a"` when re-lex
-/// // since this is one action instead of two.
-/// # let action: Action<MockTokenKind<()>> =
-/// exact(["ab", "a"]);
-/// ```
-/// To avoid the above, try [`exact_vec`] or [`exact_chars`].
 pub fn exact<ActionState: 'static, ErrorType>(
-  ss: impl Into<StringList>, // TODO: only accept one string? if user want many, use Action::or
+  s: impl Into<String>,
 ) -> Action<MockTokenKind<()>, ActionState, ErrorType> {
-  // TODO: if a string's len is 1, the action exec can just accept 1 char without any check
-  // because the head matcher is set to the first char
-
-  let ss: Vec<String> = ss.into().0;
-
-  if ss.len() == 0 {
-    panic!("empty string list");
-  }
-
-  // optimize for single string // TODO: is this needed?
-  if ss.len() == 1 {
-    let s = ss.into_iter().next().unwrap();
-    let head = s.chars().next().unwrap();
-    return Action::from(
-      simple(move |input| {
-        if input.rest().starts_with(&s) {
-          s.len()
-        } else {
-          0
-        }
-      })
-      .into(),
-    )
-    .unchecked_head_in([head]);
-  }
-
-  let heads: HashSet<_> = ss.iter().map(|s| s.chars().next().unwrap()).collect();
-  Action::from(
-    simple(move |input| {
-      for s in &ss {
-        if input.rest().starts_with(s) {
-          return s.len();
-        }
-      }
-      0 // no match
-    })
-    .into(),
-  )
-  .unchecked_head_in(heads)
+  let s: String = s.into();
+  let head = s.chars().next().unwrap();
+  return Action::from(exact_sub(s).into()).unchecked_head_in([head]);
 }
 
-/// Similar to [`exact`], but create an action for each string.
+/// Create an action for each string using [`exact`].
 ///
 /// The [`Action::head_matcher`] will be set automatically.
 /// # Examples
 /// ```
 /// # use whitehole::lexer::action::{Action, exact_vec};
-/// # let actions: Vec<Action<MockTokenKind<()>>> =
+/// # let actions: Vec<Action<_>> =
 /// exact_vec(["++", "--"]);
 /// ```
 pub fn exact_vec<ActionState: 'static, ErrorType>(
   ss: impl Into<StringList>,
 ) -> Vec<Action<MockTokenKind<()>, ActionState, ErrorType>> {
-  let ss: Vec<String> = ss.into().0;
-
-  if ss.len() == 0 {
-    panic!("empty string list");
-  }
-
-  ss.into_iter().map(|s| exact(s)).collect()
+  ss.into().0.into_iter().map(|s| exact(s)).collect()
 }
 
-/// Similar to [`exact`], but accept one string
-/// and create an action for each char.
+/// Create an action for each char using [`exact`].
 ///
 /// The [`Action::head_matcher`] will be set automatically.
 /// # Examples
 /// ```
 /// # use whitehole::lexer::action::{Action, exact_chars};
-/// # let actions: Vec<Action<MockTokenKind<()>>> =
+/// # let actions: Vec<Action<_>> =
 /// exact_chars("+-*/()");
 /// ```
 pub fn exact_chars<ActionState: 'static, ErrorType>(
   s: impl Into<String>,
 ) -> Vec<Action<MockTokenKind<()>, ActionState, ErrorType>> {
-  let s: String = s.into();
-
-  if s.len() == 0 {
-    panic!("empty string");
-  }
-
-  s.chars().map(|c| exact(c.to_string())).collect()
+  s.into().chars().map(|c| exact(c.to_string())).collect()
 }
 
 #[cfg(test)]
@@ -125,16 +74,10 @@ mod tests {
   use super::*;
   use crate::lexer::action::{ActionInput, HeadMatcher};
 
-  #[test]
-  #[should_panic]
-  fn action_utils_exact_empty() {
-    exact::<(), ()>(vec![]);
-  }
-
   fn assert_accept(action: &Action<MockTokenKind<()>>, text: &str, expected: usize) {
     assert_eq!(
       action
-        .exec(&mut ActionInput::new(text, 0, &mut ()))
+        .exec(&mut ActionInput::new(text, 0, ()).unwrap())
         .unwrap()
         .digested,
       expected
@@ -142,13 +85,12 @@ mod tests {
   }
   fn assert_reject(action: &Action<MockTokenKind<()>>, text: &str) {
     assert!(action
-      .exec(&mut ActionInput::new(text, 0, &mut ()))
+      .exec(&mut ActionInput::new(text, 0, ()).unwrap())
       .is_none());
   }
 
   #[test]
   fn action_utils_exact() {
-    // single string
     let action: Action<MockTokenKind<()>> = exact("a");
     assert_reject(&action, "b");
     assert_accept(&action, "a", 1);
@@ -159,30 +101,6 @@ mod tests {
       action.head_matcher().as_ref().unwrap(),
       HeadMatcher::OneOf(set) if set.len() == 1 && set.contains(&'a')
     ));
-
-    // multi strings
-    let action: Action<MockTokenKind<()>> = exact(["a", "b"]);
-    assert_reject(&action, "c");
-    assert_accept(&action, "a", 1);
-    assert_accept(&action, "b", 1);
-    // no lookahead
-    assert_accept(&action, "ab", 1);
-    assert_accept(&action, "ba", 1);
-    // head matcher
-    assert!(matches!(
-      action.head_matcher().as_ref().unwrap(),
-      HeadMatcher::OneOf(set) if set.len() == 2 && set.contains(&'a') && set.contains(&'b')
-    ));
-
-    // caveats
-    let action: Action<MockTokenKind<()>> = exact(["a", "ab"]);
-    assert_accept(&action, "ab", 1);
-  }
-
-  #[test]
-  #[should_panic]
-  fn action_utils_exact_vec_empty() {
-    exact_vec::<(), ()>(vec![]);
   }
 
   #[test]
