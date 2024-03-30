70f7a4c94909db91531c4d6f08f0779337ddfca0
DiscreteTom <discrete_tom@outlook.com>
2024-03-30 10:48:36 +0800

    chore(lexer)!: `regex` will return `SubAction`, update comments and unit tests

--- src/lexer/action/regex.rs
+++ src/lexer/action/regex.rs
@@ -1,83 +1,46 @@
 // TODO: only available in feature `regex`
 use super::{simple::simple, SubAction};
-use regex::{Error, Regex};
+use regex::Regex;
 
-/// Create a new action that uses a regex to match the rest of input.
-///
-/// It's recommended to set [`Action::head_matcher`] to optimize the lex performance.
+/// Create a new sub action that uses a regex to match the rest of input.
+/// Reject if the regex does not match anything.
+/// # Panics
+/// Panics if the regex is invalid.
 /// # Examples
 /// Usually the regex should start with `^` to match from the start of the rest of the input.
 /// ```
-/// # use whitehole::lexer::action::{Action, regex};
-/// # let action: Action<_, (), ()> =
-/// regex(r"^\d+").unwrap();
+/// use whitehole::lexer::action::{SubAction, regex};
+/// let a: SubAction<()> = regex(r"^\d+");
 /// ```
-/// Set [`Action::head_matcher`]:
-/// ```
-/// # use whitehole::lexer::action::Action;
-/// # use whitehole::lexer::action::regex;
-/// # use whitehole::lexer::action::HeadMatcher;
-/// # use std::collections::HashSet;
-/// # let action: Action<_> =
-/// regex(r"^abc").unwrap().unchecked_head_in(['a']);
-/// # let action: Action<_> =
-/// regex(r"^\d+").unwrap().unchecked_head_in_range('0'..='9');
-/// ```
-pub fn regex<ActionState, ErrorType>(re: &str) -> Result<SubAction<ActionState>, Error> {
-  // TODO: unwrap the result, add regex_try
-  Regex::new(re).map(|re| simple(move |input| re.find(input.rest()).map(|m| m.len()).unwrap_or(0)))
+pub fn regex<ActionState>(re: &str) -> SubAction<ActionState> {
+  Regex::new(re)
+    .map(|re| simple(move |input| re.find(input.rest()).map(|m| m.len()).unwrap_or(0)))
+    .unwrap()
 }
 
-// TODO: add this function when there is a need / good use case
-/// Create a new action that uses a regex to match the rest of input and transform the captures.
-// pub fn regex_captures<ActionState, ErrorType, T, F>(
-//   re: &str,
-//   capture_transformer: F,
-// ) -> Result<Action<MockTokenKind<T>, ActionState, ErrorType>, Error>
-// where
-//   F: Fn(Captures) -> T + 'static,
-// {
-//   Regex::new(re).map(|re| {
-//     simple_option_with_data(move |input| {
-//       re.captures(input.rest())
-//         .map(|captures| (captures[0].len(), capture_transformer(captures)))
-//     })
-//   })
-// }
+// since `regex` is based on `simple`, users could create their own regex action
+// like `regex_capture` or `regex_try`. if there are other commonly used regex actions, add them here.
 
 #[cfg(test)]
 mod tests {
   use super::*;
-  use crate::lexer::{
-    action::{input::ActionInput, output::ActionOutput},
-    token::{TokenKindId, TokenKindIdProvider},
-  };
+  use crate::lexer::action::input::ActionInput;
 
   #[test]
   fn match_at_start() {
-    let action: Action<MockTokenKind<()>> = regex(r"^\d+").unwrap();
+    let action: SubAction<()> = regex(r"^\d+");
     assert!(matches!(
-      action.exec(&mut ActionInput::new("123", 0, &mut ())),
-      Some(ActionOutput {
-        kind: mock,
-        digested: 3,
-        muted: false,
-        error: None,
-      }) if matches!(mock.data, ()) && mock.id() == &TokenKindId::new(0)
+      action.exec(&mut ActionInput::new("123", 0, ()).unwrap()),
+      Some(3)
     ));
   }
 
   #[test]
   fn match_at_middle() {
-    let action: Action<MockTokenKind<()>> = regex(r"^\d+").unwrap();
+    let action: SubAction<()> = regex(r"^\d+");
     assert!(matches!(
-      action.exec(&mut ActionInput::new("abc123", 3, &mut ())),
-      Some(ActionOutput {
-        kind: mock,
-        digested: 3,
-        muted: false,
-        error: None,
-      }) if matches!(mock.data, ()) && mock.id() == &TokenKindId::new(0)
+      action.exec(&mut ActionInput::new("abc123", 3, ()).unwrap()),
+      Some(3)
     ));
   }
 }
