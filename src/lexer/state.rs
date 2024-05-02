afe42be8c2514f7fc317a5a24d3bf5ec0acd6847
DiscreteTom <discrete_tom@outlook.com>
2024-05-02 23:15:07 +0800

    chore(lexer): update comments for `LexerState`

--- src/lexer/state.rs
+++ src/lexer/state.rs
@@ -2,6 +2,10 @@
 pub struct LexerState<'text> {
   text: &'text str,
   digested: usize,
+  // we don't store error tokens here. we store as less as possible
+  // for a better memory usage and flexibility.
+  // the generated tokens should be consumed/dropped immediately after each lex.
+  // users can store error tokens as needed by themselves as needed.
 }
 
 impl<'text> LexerState<'text> {
