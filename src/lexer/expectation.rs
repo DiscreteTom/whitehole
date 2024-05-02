557c01b15f55112b12c39ea89cfe4897ef8f8168
DiscreteTom <discrete_tom@outlook.com>
2024-05-02 22:26:28 +0800

    chore(lexer): update comments for expectation

--- src/lexer/expectation.rs
+++ src/lexer/expectation.rs
@@ -39,7 +39,7 @@ impl<'expect_text, Kind> From<&'expect_text str> for Expectation<'expect_text, K
 
 impl<'expect_text, Kind> Expectation<'expect_text, Kind> {
   /// If the [`kind`](Self::kind) is provided, the lexer will skip [`Action`](crate::lexer::action::Action)s
-  /// which are not [`muted`](crate::lexer::action::Action::muted) and have different [`kind_id`](crate::lexer::action::Action::kind_id).
+  /// with different [`kind_id`](crate::lexer::action::Action::kind_id) (unless [`muted`](crate::lexer::action::Action::muted)).
   /// # Examples
   /// ```
   /// # use whitehole::lexer::token::{token_kind};
@@ -63,10 +63,12 @@ impl<'expect_text, Kind> Expectation<'expect_text, Kind> {
 
 impl<'expect_text, Kind> Expectation<'expect_text, Kind> {
   /// If the [`literal`](Self::literal) is provided, the lexer will skip [`Action`](crate::lexer::action::Action)s
-  /// which are not [`muted`](crate::lexer::action::Action::muted) and have different [`literal`](crate::lexer::action::Action::literal).
-  ///
-  /// Be ware, we are checking the [`Action::literal`](crate::lexer::action::Action::literal),
-  /// not the [`Token::content`](crate::lexer::token::Token::content).
+  /// with different [`literal`](crate::lexer::action::Action::literal) (unless [`muted`](crate::lexer::action::Action::muted)).
+  /// # Caveats
+  /// Be ware, we are checking the [`Action::literal`](crate::lexer::action::Action::literal)
+  /// before executing an action,
+  /// not the [`Token::content`](crate::lexer::token::Token::content)
+  /// after executing an action.
   /// # Examples
   /// ```
   /// # use whitehole::lexer::token::{token_kind};
@@ -74,7 +76,7 @@ impl<'expect_text, Kind> Expectation<'expect_text, Kind> {
   /// # #[token_kind]
   /// # enum MyKind { A }
   /// # let mut expectation = Expectation::<MyKind>::default();
-  /// expectation.literal("text");
+  /// expectation.literal("import");
   /// ```
   pub fn literal(mut self, text: impl Into<&'expect_text str>) -> Self {
     self.literal = Some(text.into());
