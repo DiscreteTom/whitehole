70708a3b2fca62cc9f49a86ff0935f1eb97801a8
DiscreteTom <discrete_tom@outlook.com>
2024-05-09 21:49:30 +0800

    tests(lexer): update 02

--- tests/lexer-02-actions.rs
+++ tests/lexer-02-actions.rs
@@ -1,11 +1,10 @@
-use whitehole::lexer::{action::regex, LexerBuilder};
-use whitehole_macros::TokenKind;
-use MyKind::*; // use the enum variants directly
+use whitehole::lexer::{action::regex, token::token_kind, LexerBuilder};
 
-// define token kinds
-// make sure it implements `TokenKind` and `Clone`.
-#[derive(TokenKind, Clone)]
+// define token kinds, make sure it is decorated by `#[token_kind]`
+#[token_kind]
+#[derive(Clone, Default)]
 enum MyKind {
+  #[default]
   Anonymous,
   A,
   B,
@@ -13,77 +12,61 @@ enum MyKind {
   D,
 }
 
-// define your custom action state
-// make sure it implements `Default` and `Clone`
+#[test]
+fn action_orders() {
+  let mut lexer = LexerBuilder::new()
+    // first defined actions have higher priority
+    .define(A, regex(r"^.")) // highest priority
+    .define(B, regex(r"^."))
+    .define(C, regex(r"^.")) // lowest priority
+    .build("aa");
+
+  // lexing will always emit `A`
+  let token = lexer.lex().token.unwrap();
+  assert!(matches!(token.kind.value(), MyKind::A));
+  let token = lexer.lex().token.unwrap();
+  assert!(matches!(token.kind.value(), MyKind::A));
+}
+
+// define action state which can be shared between actions
 #[derive(Default, Clone)]
 struct MyState {
   reject: bool,
 }
 
 #[test]
-fn action_orders() {
-  let mut lexer = LexerBuilder::<MyKind, MyState>::default()
-    // first defined actions have higher priority
-    .define(A, regex(r"^a").unwrap())
-    .define(B, regex(r"^a").unwrap())
-    // different actions can share the same target token kind
-    .define(A, regex(r"^b").unwrap())
-    .build("ab");
-
-  // the first lex should be accepted as `A`
-  let token = lexer.lex().token.unwrap();
-  assert!(matches!(token.kind, A));
-
-  // the second lex should be accepted as `A` too
-  let token = lexer.lex().token.unwrap();
-  assert!(matches!(token.kind, A));
-}
-
-#[test]
 fn action_decorators() {
-  let mut lexer = LexerBuilder::<MyKind, MyState, &str>::default()
-    // when using action decorators
-    // rust compiler can't infer the action's generic parameters
-    // so we need to use `define_with`, `append_with` and `ignore_with` to define actions
-    // these methods accept a function which takes an `ActionBuilder` as its parameter
-    // so the action's generic parameters can be inferred from the `ActionBuilder`
-    .define(
+  let mut lexer = LexerBuilder::<_, MyState, _>::default()
+    // you can use `define_with` to apply a decorator to all the actions in the `define` call
+    .define_with(
       Anonymous,
-      // to mute an action, we can use `mute` or `mute_if`
-      regex(r"^\s+").unwrap().mute(true),
+      // to mute an action (just like `LexerBuilder::ignore`), you can use `mute`
+      regex(r"^\s+"),
+      |a| a.mute(),
     )
     .define_with(
       A,
-      // to set token's error, we can use `check` or `error`
-      regex(r"^a").unwrap(),
-      |a| {
-        a.check(|ctx| {
-          if ctx.output.rest().len() > 0 {
-            Some("error")
-          } else {
-            None
-          }
-        })
-      },
+      // to set token's error, you can use `check` or `error`
+      regex(r"^a"),
+      |a| a.error("error"),
     )
-    .define(
+    .define_with(
       B,
-      // to reject an action after the output is yielded, we can use `reject` or `reject_if`
-      regex(r"^b")
-        .unwrap()
-        .reject_if(|ctx| ctx.output.rest().len() > 0),
+      // to reject an action after the action is executed and accepted, you can use `reject` or `reject_if`
+      regex(r"^b"),
+      |a| a.reject_if(|ctx| ctx.rest().len() > 0),
     )
     .define_with(
       C,
-      // to reject an action before the output is yielded, we can use `prevent`
-      regex(r"^c").unwrap(),
+      // to reject an action before the action is executed, you can use `prevent`
+      regex(r"^c"),
       |a| a.prevent(|input| input.state.reject),
     )
     .define_with(
       D,
-      // use `then` to run a callback if this action is accepted and is not a peek
+      // use `then` to run a callback if this action is accepted
       // this is usually used to modify lexer's action state
-      regex(r"^d").unwrap(),
+      regex(r"^d"),
       |a| {
         a.callback(|ctx| {
           ctx.input.state.reject = true;
@@ -97,7 +80,7 @@ fn action_decorators() {
   // the first lex should be accepted but with error set
   let res = lexer.lex();
   let token = res.token.unwrap();
-  assert!(matches!(token.kind, A));
+  assert!(matches!(token.kind.value(), MyKind::A));
   assert!(matches!(token.error, Some("error")));
   assert_eq!(res.digested, 1);
   // res.token is not included in res.errors even if the token has error
@@ -114,13 +97,13 @@ fn action_decorators() {
 
   // the first lex should be accepted
   let token = lexer.lex().token.unwrap();
-  assert!(matches!(token.kind, C));
+  assert!(matches!(token.kind.value(), MyKind::C));
   assert_eq!(token.range.start, 0);
   assert_eq!(token.range.end, 1);
 
   // the second lex should be accepted and will change the state
   let token = lexer.lex().token.unwrap();
-  assert!(matches!(token.kind, D));
+  assert!(matches!(token.kind.value(), MyKind::D));
   assert_eq!(token.range.start, 2);
   assert_eq!(token.range.end, 3);
   assert_eq!(lexer.action_state.reject, true);
