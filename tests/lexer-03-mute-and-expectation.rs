47a65c47dacd80229aba12a3cbf25bbfe8b937e1
DiscreteTom <discrete_tom@outlook.com>
2024-05-10 19:24:15 +0800

    tests(lexer): update 03

--- tests/lexer-03-mute-and-expectation.rs
+++ tests/lexer-03-mute-and-expectation.rs
@@ -1,15 +1,13 @@
 use whitehole::lexer::{
-  action::regex, expectation::Expectation, token::TokenKind, Action, LexerBuilder,
+  action::{exact, regex, whitespaces, Action},
+  token::{token_kind, SubTokenKind},
+  LexerBuilder,
 };
-use whitehole_macros::TokenKind;
-use MyKind::*; // use the enum variants directly
 
-// define token kinds
-// make sure it implements `TokenKind` and `Clone`.
-#[derive(TokenKind, Clone, Default)]
+// define token kinds, make sure it is decorated by `#[token_kind]`
+#[token_kind]
+#[derive(Clone, Default)]
 enum MyKind {
-  // as a convention, we use `Anonymous` as the default kind
-  // so that we can use `builder.ignore_default`
   #[default]
   Anonymous,
   A,
@@ -17,107 +15,57 @@ enum MyKind {
 }
 
 #[test]
-fn maybe_muted() {
-  // there is a field `maybe_muted` in `Action`
-  // which will be set if you use `action.mute` or `action.mute_if`
-  let action: Action<()> = regex(r"^a").unwrap();
-  // by default `maybe_muted` is `false`
-  assert!(!action.maybe_muted);
-  // with `mute`, the `maybe_muted` field will be set by the argument
-  let action = action.mute(true);
-  assert!(action.maybe_muted);
-  let action = action.mute(false);
-  assert!(!action.maybe_muted);
-
-  // with `mute_if`, the `maybe_muted` field will always be set to `true`
-  let action = action.mute_if(|_| true);
-  assert!(action.maybe_muted);
-  let action = action.mute_if(|_| false);
-  assert!(action.maybe_muted);
-
-  // we can edit the `maybe_muted` field if we know what we are doing
-  let mut action: Action<()> = regex(r"^a").unwrap();
-  action.maybe_muted = true;
-  assert!(action.maybe_muted);
-}
-
-#[test]
-fn builder_ignore() {
-  // when you use `builder.ignore` or `builder.ignore_with`
-  // the builder will set the `maybe_muted` field to `true`
-  assert!(
-    LexerBuilder::<MyKind>::default()
-      .ignore(regex("^-").unwrap().bind(Anonymous))
-      .build_stateless()
-      .actions()[0]
-      .maybe_muted
-  );
-  assert!(
-    LexerBuilder::<MyKind>::default()
-      .ignore(regex("^-").unwrap().bind(Anonymous))
-      .build_stateless()
-      .actions()[0]
-      .maybe_muted
-  );
-
-  // if your token kind implements `Default` and `Clone`
-  // you can use `builder.ignore_default` or `builder.ignore_default_with`
-  // so that the builder will bind the A with the default kind
-  let stateless = LexerBuilder::<MyKind>::default()
-    .ignore_default(regex("^-").unwrap())
-    .build_stateless();
-  let action = &stateless.actions()[0];
-  assert!(action.maybe_muted);
-  assert_eq!(action.possible_kinds().len(), 1);
-  assert!(action.possible_kinds().contains(&Anonymous.id()));
-  let stateless = LexerBuilder::<MyKind>::default()
-    .ignore_default(regex("^-").unwrap())
-    .build_stateless();
-  let action = &stateless.actions()[0];
-  assert!(action.maybe_muted);
-  assert_eq!(action.possible_kinds().len(), 1);
-  assert!(action.possible_kinds().contains(&Anonymous.id()));
+fn muted_actions() {
+  // there is a field `muted` in `Action`
+  let action: Action<_> = regex(r"^a").into_action();
+  // by default `muted` is `false`
+  assert!(!action.muted());
+  // with `mute`, the `muted` field will be set to `true`
+  let action = action.mute();
+  assert!(action.muted());
 }
 
 #[test]
 fn expectation() {
-  let mut lexer = LexerBuilder::<MyKind>::default()
-    .ignore(regex("^-").unwrap().bind(Anonymous))
-    .define(A, regex(r"a").unwrap())
-    .define(B, regex(r"a").unwrap())
-    .build("-a");
+  let mut lexer = LexerBuilder::new()
+    // with `ignore`, the `LexerBuilder` will make actions muted
+    .ignore_default(whitespaces())
+    .define(A, exact("a"))
+    .define(B, exact("a"))
+    .define(B, exact("b"))
+    .build("\ta");
 
   // by default, the lex will evaluate all actions in the order they are defined
-  // so the first lex should be accepted as `A`
+  // so the lex should be accepted as `A`
   let token = lexer.lex().token.unwrap();
-  assert!(matches!(token.kind, A));
+  assert!(matches!(token.kind.value(), MyKind::A));
 
   // but if we have an expected kind
-  // the lex will only evaluate actions which are bound to the expected kind
-  // or maybe-muted actions
-  let mut lexer = lexer.reload("-a");
-  let res = lexer.lex_expect(&B);
+  // the lex will only evaluate actions
+  // which are bound to the expected kind or muted
+  let mut lexer = lexer.reload("\ta");
+  let res = lexer.lex_with(|o| o.expect(B::kind_id()));
   let token = res.token.unwrap();
-  assert!(matches!(token.kind, B));
-  assert_eq!(res.digested, 2); // the muted action is also evaluated and digested a character
+  assert!(matches!(token.kind.value(), MyKind::B));
+  assert_eq!(res.digested, 2); // the muted action was also evaluated and digested a byte
 
-  // we can also expect a specific text
-  let mut lexer = lexer.reload("-a");
-  let res = lexer.lex_expect("b");
+  // we can also expect a literal if the literal appears in `exact` or `word`
+  let mut lexer = lexer.reload("\ta");
+  let res = lexer.lex_with(|o| o.expect("b"));
   let token = res.token;
   assert!(token.is_none());
-  assert_eq!(res.digested, 1); // the muted action is also evaluated and digested a character
-  let token = lexer.lex_expect("a").token.unwrap();
-  assert!(matches!(token.kind, A));
+  assert_eq!(res.digested, 1); // the muted action is also evaluated and digested a byte
+  let token = lexer.lex_with(|o| o.expect("a")).token.unwrap();
+  assert!(matches!(token.kind.value(), MyKind::A));
 
   // or both the text and the kind are expected
-  let mut lexer = lexer.reload("-a");
+  let mut lexer = lexer.reload("\ta");
   assert!(lexer
-    .lex_expect(Expectation::from("b").kind(A))
+    .lex_with(|o| o.expect_with(|e| e.literal("b").kind(A)))
     .token
     .is_none());
   assert!(lexer
-    .lex_expect(Expectation::from("a").kind(A))
+    .lex_with(|o| o.expect_with(|e| e.literal("a").kind(A)))
     .token
     .is_some());
 }
