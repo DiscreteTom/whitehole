5f7d40e60249cd2b34688832fae473111587020a
DiscreteTom <discrete_tom@outlook.com>
2024-05-12 10:08:30 +0800

    feat(lexer): add `LexerState::trimmed`, `LexerState::trim`

--- src/lexer/state.rs
+++ src/lexer/state.rs
@@ -4,6 +4,8 @@ pub struct LexerState<'text> {
   text: &'text str,
   /// See [`Self::digested`].
   digested: usize,
+  /// See [`Self::trimmed`].
+  trimmed: bool,
   // we don't store error tokens here. we store as less as possible
   // for a better memory usage and flexibility.
   // the generated tokens should be consumed/dropped immediately after each lex.
@@ -14,7 +16,11 @@ impl<'text> LexerState<'text> {
   /// Create a new lexer state with the given text.
   /// [`Self::digested`] will be 0.
   pub fn new(text: &'text str) -> Self {
-    LexerState { text, digested: 0 }
+    LexerState {
+      text,
+      digested: 0,
+      trimmed: text.len() == 0,
+    }
   }
 
   /// The whole input text.
@@ -25,6 +31,10 @@ impl<'text> LexerState<'text> {
   pub fn digested(&self) -> usize {
     self.digested
   }
+  /// Whether the text is trimmed.
+  pub fn trimmed(&self) -> bool {
+    self.trimmed
+  }
 
   /// Get the rest of the text which is not digested.
   pub fn rest(&self) -> &'text str {
@@ -32,8 +42,16 @@ impl<'text> LexerState<'text> {
   }
 
   /// Digest `n` bytes.
-  /// The caller should ensure `n` is valid.
+  /// The caller should ensure `n` is smaller than the rest text length.
   pub fn digest(&mut self, n: usize) {
     self.digested += n;
+    self.trimmed = self.digested == self.text.len();
+  }
+
+  /// Digest `n` bytes and set [`Self::trimmed`] to `true`.
+  /// The caller should ensure `n` is smaller than the rest text length.
+  pub fn trim(&mut self, n: usize) {
+    self.digested += n;
+    self.trimmed = true;
   }
 }
