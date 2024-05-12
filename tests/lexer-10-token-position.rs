cf01106e3941692da2cd2694c19d209332b73d84
DiscreteTom <discrete_tom@outlook.com>
2024-05-12 22:29:13 +0800

    tests(lexer): update 10

--- tests/lexer-10-token-position.rs
+++ tests/lexer-10-token-position.rs
@@ -1,51 +1,56 @@
 use whitehole::lexer::{
   action::regex,
   position::{Position, PositionTransformer},
-  token::Range,
+  token::{token_kind, Range},
   LexerBuilder,
 };
-use whitehole_macros::TokenKind;
-use MyKind::*; // use the enum variants directly
 
-// define token kinds
-// make sure it implements `TokenKind` and `Clone`.
-#[derive(TokenKind, Clone)]
+// define token kinds, make sure it is decorated by `#[token_kind]`
+#[token_kind]
+#[derive(Clone, Default)]
 enum MyKind {
+  #[default]
   Anonymous,
   A,
 }
 
 #[test]
 fn token_position() {
-  // for a better performance, lexer doesn't keep track of the position of each token.
-  // token is ephemeral, it has information about the index of the buffer when it is created.
-
+  // for a better performance, the position of each token is not tracked by default.
   // to get the position of a token (instead of an index)
-  // we can use PositionTransformer
+  // you can use PositionTransformer
   let text = "123\n123\n";
-  // the position transformer will calculate the index of each line
-  // so we can use it to get the position of a token
+  // when creating a PositionTransformer it will calculate the range in byte of each line
   let pt = PositionTransformer::new(text);
 
-  let lexer = LexerBuilder::<MyKind>::default()
-    .ignore(regex(r"^\n").unwrap().bind(Anonymous))
-    .define(A, regex(r"^123").unwrap())
+  let lexer = LexerBuilder::new()
+    .ignore_default(regex(r"^\n"))
+    .define(A, regex(r"^123"))
     .build(text);
 
   let (output, _) = lexer.peek();
   let token = output.token.unwrap();
 
-  // use `transform` to get the position from the index
-  // it will use binary search to find the line index
-  let position = pt.transform(token.range.start).unwrap();
-  assert!(matches!(position, Position { line: 1, column: 1 }));
+  // use `transform` to get the position from the index,
+  // PositionTransformer will use binary search to find the line index
+  // and calculate the character index by counting the characters in the line
+  let position = pt.transform(token.range.start, text).unwrap();
+  assert!(matches!(
+    position,
+    Position {
+      line: 0,
+      character: 0
+    }
+  ));
 
-  // however, if we want to batch transform the positions of all tokens
+  // however, if you want to batch transform the positions of all tokens,
   // calling `transform` for each token is not efficient
-  // we can get the line ranges and calculate them by ourselves
+  // you can get the line ranges and calculate them by yourself
   let line_ranges = pt.line_ranges();
   assert_eq!(line_ranges.len(), 3);
   assert!(matches!(line_ranges[0], Range { start: 0, end: 4 }));
   assert!(matches!(line_ranges[1], Range { start: 4, end: 8 }));
   assert!(matches!(line_ranges[2], Range { start: 8, end: 8 }));
+
+  // you can also do this in parallel or using multiple threads to improve the performance
 }
