23516e8c494a1d5aa39c227b5ea69e3a6f5c5cd3
DiscreteTom <discrete_tom@outlook.com>
2024-05-12 17:20:23 +0800

    feat(lexer): add `LexerBuilder::stateful`, `LexerBuilder::with_error`, `LexerBuilder::stateful_with_error`

--- src/lexer/builder.rs
+++ src/lexer/builder.rs
@@ -8,6 +8,8 @@ pub use action_list::*;
 use super::{action::Action, stateless::StatelessLexer, Lexer};
 use std::rc::Rc;
 
+/// To create this, see [`Self::new`], [`Self::stateful`],
+/// [`Self::with_error`] and [`Self::stateful_with_error`].
 pub struct LexerBuilder<Kind: 'static, ActionState = (), ErrorType = ()> {
   actions: Vec<Action<Kind, ActionState, ErrorType>>,
 }
@@ -21,9 +23,85 @@ impl<Kind, ActionState, ErrorType> Default for LexerBuilder<Kind, ActionState, E
 }
 
 impl<Kind> LexerBuilder<Kind, (), ()> {
-  /// Create a new lexer builder with no `ActionState` and `ErrorType`.
+  /// Create a new lexer builder, set `ActionState` and `ErrorType` to `()`.
+  /// # Examples
+  /// ```
+  /// # use whitehole::lexer::{LexerBuilder, action::exact};
+  /// # let mut builder =
+  /// LexerBuilder::new();
+  /// # builder.append(exact("a"));
+  /// // equals to
+  /// # let mut builder =
+  /// LexerBuilder::<_>::default();
+  /// # builder.append(exact("a"));
+  /// # let mut builder =
+  /// LexerBuilder::<_, (), ()>::default();
+  /// # builder.append(exact("a"));
+  /// ```
   pub fn new() -> Self {
     Self::default()
+  }
+
+  /// Create a new lexer builder with the provided `ActionState`,
+  /// set `ErrorType` to `()`.
+  /// # Examples
+  /// ```
+  /// # use whitehole::lexer::{LexerBuilder, action::exact};
+  /// # struct MyState;
+  /// # let mut builder =
+  /// LexerBuilder::stateful::<MyState>();
+  /// # builder.append(exact("a"));
+  /// // equals to
+  /// # let mut builder =
+  /// LexerBuilder::<_, MyState>::default();
+  /// # builder.append(exact("a"));
+  /// # let mut builder =
+  /// LexerBuilder::<_, MyState, ()>::default();
+  /// # builder.append(exact("a"));
+  /// ```
+  pub fn stateful<ActionState>() -> LexerBuilder<Kind, ActionState, ()> {
+    LexerBuilder::default()
+  }
+}
+
+impl<Kind, ErrorType> LexerBuilder<Kind, (), ErrorType> {
+  /// Create a new lexer builder, set `ActionState` to `()`,
+  /// infer `ErrorType` from the provided actions.
+  /// # Examples
+  /// ```
+  /// # use whitehole::lexer::{LexerBuilder, action::exact};
+  /// # #[derive(Clone)]
+  /// # struct MyError;
+  /// # let mut builder =
+  /// LexerBuilder::with_error();
+  /// # builder.append_with(exact("a"), |a| a.error(MyError));
+  /// // equals to
+  /// # let mut builder =
+  /// LexerBuilder::<_, (), _>::default();
+  /// # builder.append_with(exact("a"), |a| a.error(MyError));
+  /// ```
+  pub fn with_error() -> Self {
+    Self::default()
+  }
+
+  /// Create a new lexer builder with the provided `ActionState`,
+  /// infer `ErrorType` from the provided actions.
+  /// # Examples
+  /// ```
+  /// # use whitehole::lexer::{LexerBuilder, action::exact};
+  /// # struct MyState;
+  /// # #[derive(Clone)]
+  /// # struct MyError;
+  /// # let mut builder =
+  /// LexerBuilder::stateful_with_error::<MyState>();
+  /// # builder.append_with(exact("a"), |a| a.error(MyError));
+  /// // equals to
+  /// # let mut builder =
+  /// LexerBuilder::<_, MyState, _>::default();
+  /// # builder.append_with(exact("a"), |a| a.error(MyError));
+  /// ```
+  pub fn stateful_with_error<ActionState>() -> LexerBuilder<Kind, ActionState, ErrorType> {
+    LexerBuilder::default()
   }
 }
 
