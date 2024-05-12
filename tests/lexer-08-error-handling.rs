64a8944db9176961a03c8422ef6678b6c6dbbda4
DiscreteTom <discrete_tom@outlook.com>
2024-05-12 17:32:14 +0800

    tests(lexer): update 08

--- tests/lexer-08-error-handling.rs
+++ tests/lexer-08-error-handling.rs
@@ -1,63 +1,66 @@
-use whitehole::lexer::{action::regex, LexerBuilder};
-use whitehole_macros::TokenKind;
-use MyKind::*; // use the enum variants directly
+use whitehole::lexer::{
+  action::{regex, whitespaces},
+  token::token_kind,
+  LexerBuilder,
+};
 
-// define token kinds
-// make sure it implements `TokenKind` and `Clone`.
-#[derive(TokenKind, Clone)]
+// define token kinds, make sure it is decorated by `#[token_kind]`
+#[token_kind]
+#[derive(Clone, Default)]
 enum MyKind {
+  #[default]
   Anonymous,
   A,
 }
 
 #[test]
 fn error_tokens() {
-  // we can specify the error type as the 3rd generic parameter in `Builder`
-  // this is acceptable errors, the lexing process won't stop
+  // tokens can have errors,
+  // these are acceptable errors and won't stop the lexing loop
 
-  // in this example we use `&str` as the error type
-  let lexer = LexerBuilder::<MyKind, (), &str>::default()
-    .ignore_with(regex(r"^\s+").unwrap().bind(Anonymous), |a| {
+  // in this example we use `&str` as the error type.
+  // use `LexerBuilder::with_error` to auto infer the error type
+  let lexer = LexerBuilder::with_error()
+    .ignore_default_with(whitespaces(), |a| {
       a
-        // set error by using `error`
+        // set the error by using `error`
         .error("ignored")
-        .into()
     })
-    .define_with(A, regex(r"^a").unwrap(), |a| {
-      // set error by using `check`
+    .define_with(A, regex(r"^a"), |a| {
+      // set the error by using `check`
       a.check(|ctx| {
-        if ctx.output.rest().len() == 0 {
+        if ctx.rest().len() == 0 {
           Some("end")
         } else {
           None
         }
       })
-      .into()
     })
     .build(" a");
 
   // when `lex`, `peek` or `trim`, we can get error tokens from the output
   let (output, _) = lexer.peek();
-  // now even if the whitespace is muted, it contains an error
+  // even if the whitespace is muted, it contains an error.
+  // error tokens will be collected in the output even they are muted,
   // so we can get the error token from the peek result
-  // be ware: `errors` in the result doesn't contain the peeked token
   assert_eq!(output.errors.len(), 1);
-  assert!(matches!(output.errors[0].kind, Anonymous));
+  assert!(matches!(output.errors[0].kind.value(), MyKind::Anonymous));
   assert!(matches!(output.errors[0].error, Some("ignored")));
-  // we can still get the peeked (error) token
+  // we can still get the peeked token, which is also an error token
+  // but it is not muted
   let token = output.token.unwrap();
-  assert!(matches!(token.kind, A));
+  assert!(matches!(token.kind.value(), MyKind::A));
   assert!(matches!(token.error, Some("end")));
 }
 
 #[test]
 fn panic_mode() {
-  // for some unacceptable errors, we can use panic mode
+  // for some unacceptable errors, you can use "panic mode"
   // which will digest 1 char and try again
 
-  let mut lexer = LexerBuilder::<MyKind, (), &str>::default()
-    .ignore(regex(r"^\s+").unwrap().bind(Anonymous))
-    .define(A, regex(r"^a").unwrap())
+  let mut lexer = LexerBuilder::new()
+    .ignore_default(regex(r"^\s+"))
+    .define(A, regex(r"^a"))
     .build("b a");
 
   // in this case when we peek the lexer
@@ -67,12 +70,12 @@ fn panic_mode() {
   assert!(output.token.is_none());
   assert_eq!(output.digested, 0);
 
-  // enter panic mode, take 1 char and try again
-  // this will reset the lexer's action state, unless we provide a new state
-  lexer.take(1);
+  // enter panic mode, digest 1 char directly and try again
+  // this will reset the lexer's action state, unless you provide a new action state
+  lexer.digest(1);
   // now we can peek
   let (output, _) = lexer.peek();
-  assert!(matches!(output.token.unwrap().kind, A));
+  assert!(matches!(output.token.unwrap().kind.value(), MyKind::A));
   assert_eq!(output.digested, 2);
 
   // further more, if you know what you are doing
