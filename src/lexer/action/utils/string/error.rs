370b065c7a02796c4fe8fe447c15f587021a698f
DiscreteTom <discrete_tom@outlook.com>
2024-07-13 11:34:37 +0800

    chore(lexer): update comments for StringLiteralError

--- src/lexer/action/utils/string/error.rs
+++ src/lexer/action/utils/string/error.rs
@@ -1,7 +1,10 @@
 /// Errors when lexing a string.
 #[derive(Clone, Debug, PartialEq)]
 pub enum StringLiteralError<CustomError> {
+  /// The string is unterminated.
+  /// E.g. `"hello`
   Unterminated,
+  /// An escape sequence is not handled by any escape handler.
   UnhandledEscape,
   Custom(CustomError),
 }
