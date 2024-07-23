7beff469b917874173114a1bbe125b511c03fe35
DiscreteTom <discrete_tom@outlook.com>
2024-07-23 18:06:40 +0800

    chore(macro): optimize code

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -62,14 +62,14 @@ pub fn token_kind(_attr: TokenStream, input: TokenStream) -> TokenStream {
 /// Same as [`token_kind`].
 /// This is only used internally in whitehole.
 #[proc_macro_attribute]
-pub fn _token_kind(_attr: TokenStream,input: TokenStream) -> TokenStream {
+pub fn _token_kind(_attr: TokenStream, input: TokenStream) -> TokenStream {
   common(quote! { crate }, input).into()
 }
 
 /// Print the generated code for debugging.
 /// This is only used internally in whitehole.
 #[proc_macro_attribute]
-pub fn _debug_token_kind(_attr: TokenStream,input: TokenStream) -> TokenStream {
+pub fn _debug_token_kind(_attr: TokenStream, input: TokenStream) -> TokenStream {
   let ts = common(quote! { crate }, input);
   println!("{}", ts.to_string());
   ts.into()
@@ -91,21 +91,24 @@ fn common(crate_name: proc_macro2::TokenStream, input: TokenStream) -> proc_macr
   let mut gen = Vec::new();
 
   // override the original enum
-  let generated_fields: Vec<_> = variants.iter().map(|variant| {
-    let variant_name = &variant.ident;
-    let variant_attrs = &variant.attrs;
-    if matches!(variant.fields, Fields::Unit) {
-      // for unit variants, we don't need to wrap them in unnamed fields.
-      // with this design, we can make `#[derive(Default)]` and `#[default]` working
-      // because `#[default]` only works for unit fields.
-      quote! { #(#variant_attrs)* #variant_name, }
-    } else {
-      quote! { #(#variant_attrs)* #variant_name(#variant_name), }
-    }
-  }).collect();
+  let generated_fields: Vec<_> = variants
+    .iter()
+    .map(|variant| {
+      let variant_name = &variant.ident;
+      let variant_attrs = &variant.attrs;
+      if matches!(variant.fields, Fields::Unit) {
+        // for unit variants, we don't need to wrap them in unnamed fields.
+        // with this design, we can make `#[derive(Default)]` and `#[default]` working
+        // because `#[default]` only works for unit fields.
+        quote! { #(#variant_attrs)* #variant_name, }
+      } else {
+        quote! { #(#variant_attrs)* #variant_name(#variant_name), }
+      }
+    })
+    .collect();
   gen.push(quote! { #(#attrs)* #vis enum #enum_name { #(#generated_fields)* } });
 
-  // generate a struct for each variant
+  // generate a struct for each variant and implement traits for them
   variants.iter().enumerate().for_each(|(index, variant)| {
     let variant_name = &variant.ident;
     let variant_attrs = &variant.attrs;
@@ -122,12 +125,7 @@ fn common(crate_name: proc_macro2::TokenStream, input: TokenStream) -> proc_macr
           })
           .collect();
         gen.push(quote! {
-          #(#attrs)* pub struct #variant_name{ #(#generated_fields),* }
-          impl Into<#enum_name> for #variant_name {
-            fn into(self) -> #enum_name {
-              #enum_name::#variant_name(self)
-            }
-          }
+          #(#attrs)* pub struct #variant_name{ #(#generated_fields),* } // no semicolon at the end
         });
       }
       Fields::Unnamed(fields) => {
@@ -141,6 +139,20 @@ fn common(crate_name: proc_macro2::TokenStream, input: TokenStream) -> proc_macr
           .collect();
         gen.push(quote! {
           #(#attrs)* pub struct #variant_name(#(#generated_fields),*);
+        });
+      }
+      Fields::Unit => {
+        gen.push(quote! {
+          #(#attrs)* pub struct #variant_name;
+        });
+      }
+    }
+
+    // impl Into<MyKind> for the generated structs
+    // this is required by `TokenKindIdBinding::new`
+    match &variant.fields {
+      Fields::Named(_) | Fields::Unnamed(_) => {
+        gen.push(quote! {
           impl Into<#enum_name> for #variant_name {
             fn into(self) -> #enum_name {
               #enum_name::#variant_name(self)
@@ -150,7 +162,6 @@ fn common(crate_name: proc_macro2::TokenStream, input: TokenStream) -> proc_macr
       }
       Fields::Unit => {
         gen.push(quote! {
-          #(#attrs)* pub struct #variant_name;
           impl Into<#enum_name> for #variant_name {
             fn into(self) -> #enum_name {
               #enum_name::#variant_name
@@ -160,6 +171,8 @@ fn common(crate_name: proc_macro2::TokenStream, input: TokenStream) -> proc_macr
       }
     }
 
+    // impl Into<TokenKindIdBinding<MyKind>> for the generated structs
+    // this is required by `Action::select`
     gen.push(quote! {
       impl Into<#crate_name::lexer::token::TokenKindIdBinding<#enum_name>> for #variant_name {
         fn into(self) -> #crate_name::lexer::token::TokenKindIdBinding<#enum_name> {
@@ -168,7 +181,7 @@ fn common(crate_name: proc_macro2::TokenStream, input: TokenStream) -> proc_macr
       }
     });
 
-    // impl SubTokenKind for the generated struct
+    // impl SubTokenKind and Into<TokenKindId<TokenKindIdBinding<MyKind>>> for the generated struct
     // we should impl SubTokenKind for `TokenKindIdBinding<MyKind>` instead of `MyKind`
     // because for `Action<TokenKindIdBinding<MyKind>>` the `action.kind_id` should be
     // `TokenKindId<TokenKindIdBinding<MyKind>>` instead of `TokenKindId<MyKind>`
@@ -176,19 +189,20 @@ fn common(crate_name: proc_macro2::TokenStream, input: TokenStream) -> proc_macr
     let token_kind_id_const = syn::Ident::new(&format!("_TOKEN_KIND_ID_{}", index), Span::call_site());
     let sub_token_kind_name = syn::LitStr::new(&variant_name.to_string(), Span::call_site());
     gen.push(quote! {
+      // use a private mod to hide the constants
       mod #mod_name {
-        use super::*;
+        use super::*; // TODO: replace with the actual name instead of '*' to avoid potential conflicts
         use #crate_name::lexer::token::{SubTokenKind, TokenKindId, TokenKindIdBinding};
         const #token_kind_id_const: TokenKindId<TokenKindIdBinding<#enum_name>> = TokenKindId::new(#index, #sub_token_kind_name);
+        // impl SubTokenKind so users can get the kind id from the type instead of the value
         impl SubTokenKind<TokenKindIdBinding<#enum_name>> for #variant_name {
-          fn kind_id() -> 
-            &'static TokenKindId<
-              TokenKindIdBinding<#enum_name>
-            >
-          {
+          fn kind_id() -> &'static TokenKindId<TokenKindIdBinding<#enum_name>> {
             &#token_kind_id_const
           }
         }
+        // impl Into<TokenKindId<TokenKindIdBinding<MyKind>>> for the generated struct
+        // this is helpful in expectational lexing, if users wants to provide the expected kind id
+        // they can just use the value (especially for unit variants)
         impl Into<&'static TokenKindId<TokenKindIdBinding<#enum_name>>> for #variant_name {
           fn into(self) -> &'static TokenKindId<TokenKindIdBinding<#enum_name>> {
             &#token_kind_id_const
