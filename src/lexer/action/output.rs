4ee230baa18439b2cead5ec8f00bb7deab837b5a
DiscreteTom <discrete_tom@outlook.com>
2024-07-24 21:33:17 +0800

    chore(lexer): update comments for `ActionOutput`

--- src/lexer/action/output.rs
+++ src/lexer/action/output.rs
@@ -1,12 +1,13 @@
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct ActionOutput<Kind, OptionErrorType> {
+  /// The [`Token::kind`](crate::lexer::token::Token::kind)
+  /// that is created by this action.
   pub kind: Kind,
   /// How many bytes are digested by this action.
   /// `0` is allowed, but be careful with infinite loops.
   pub digested: usize,
   /// If [`Some`], the action is still accepted,
-  /// and a token will be created even if the output is muted.
-  /// Muted error tokens will be collected in
+  /// and the error will be collected by
   /// [`LexOutput::errors`](crate::lexer::output::LexOutput::errors).
   pub error: OptionErrorType, // this will be `Option<ErrorType>` or `&Option<ErrorType>`
 }
