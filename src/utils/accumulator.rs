21eccf848b5cddea7323cc0161a1e2eca43eb71b
DiscreteTom <discrete_tom@outlook.com>
2024-07-25 19:57:48 +0800

    chore(utils): update comments for `Accumulator`

--- src/utils/accumulator.rs
+++ src/utils/accumulator.rs
@@ -1,4 +1,9 @@
 /// Accumulate values.
+///
+/// These types already implement the [`Accumulator`] trait:
+/// - `()` - mock accumulator that does nothing.
+/// - [`Vec<T>`] - accumulate values into a vector.
+/// - [`String`] - accumulate characters or strings into a string.
 pub trait Accumulator<T> {
   /// Update the accumulator with a value.
   fn update(&mut self, t: T);
