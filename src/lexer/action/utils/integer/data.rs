b9ae80a6855beff1b4be678a42b9fe277ffafe40
DiscreteTom <discrete_tom@outlook.com>
2024-07-25 20:26:58 +0800

    chore(lexer): update comments for `IntegerLiteralData`

--- src/lexer/action/utils/integer/data.rs
+++ src/lexer/action/utils/integer/data.rs
@@ -1,6 +1,11 @@
 #[derive(Default, Debug, Clone)]
 pub struct IntegerLiteralData<Sep, Value> {
   /// The byte index of numeric separators in the integer literal body.
+  /// This field is set if you enable numeric separators and provide an accumulator.
+  /// See [`IntegerLiteralBodyOptions::separator`](crate::lexer::action::utils::integer::IntegerLiteralBodyOptions::separator).
   pub separators: Sep,
+  /// The accumulated value of the integer literal body.
+  /// This field is set if you provide an accumulator.
+  /// See [`IntegerLiteralBodyOptions::value_to`](crate::lexer::action::utils::integer::IntegerLiteralBodyOptions::value_to).
   pub value: Value,
 }
