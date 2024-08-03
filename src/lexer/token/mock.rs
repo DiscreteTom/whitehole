3f588a8710e2de07783de3d05135bfad5a22c20d
DiscreteTom <discrete_tom@outlook.com>
2024-08-04 00:02:13 +0800

    chore(lexer): simplify code for `MockTokenKind`

--- src/lexer/token/mock.rs
+++ src/lexer/token/mock.rs
@@ -1,34 +1,30 @@
 use super::{SubTokenKind, TokenKindId, TokenKindIdBinding};
 use std::mem::transmute;
 
-/// This implements [`SubTokenKind`]
+/// This implements [`SubTokenKind`] and `Into<TokenKindIdBinding<MockTokenKind<T>>>`
 /// and only has one possible token kind id value.
 /// This is useful as a placeholder or data carrier.
 /// # Examples
 /// ```
 /// use whitehole::lexer::token::{MockTokenKind, SubTokenKind, TokenKindIdBinding, TokenKindIdProvider};
+///
 /// let v1: TokenKindIdBinding<MockTokenKind<i32>> = MockTokenKind::new(42).into();
 /// let v2: TokenKindIdBinding<MockTokenKind<bool>> = MockTokenKind::new(true).into();
+///
 /// assert_eq!(v1.id(), MockTokenKind::kind_id());
 /// assert_eq!(v2.id(), MockTokenKind::kind_id());
 /// ```
 #[derive(Clone, Debug, PartialEq, Eq, Hash, Default)]
 pub struct MockTokenKind<T> {
+  /// The data carried by the [`MockTokenKind`].
   pub data: T,
 }
 
-/// The only possible kind id of [`MockTokenKind`].
-const MOCK_TOKEN_KIND_ID: TokenKindId<MockTokenKind<()>> = TokenKindId::new(0, "");
-
-/// Since all `TokenKindId<MockTokenKind<T>>` have the same memory layout,
-/// it should be safe to cast it to any `TokenKindId<MockTokenKind<T>>`.
-#[inline]
-const fn cast_mock_token_kind_id<T>() -> &'static TokenKindId<MockTokenKind<T>> {
-  unsafe { transmute(&MOCK_TOKEN_KIND_ID) }
-}
-
 impl<T> MockTokenKind<T> {
-  /// Create a new `MockTokenKind` with the given data.
+  /// The only possible kind id of [`MockTokenKind`].
+  const KIND_ID: TokenKindId<MockTokenKind<()>> = TokenKindId::new(0, "");
+
+  /// Create a new instance with the given data.
   #[inline]
   pub const fn new(data: T) -> Self {
     Self { data }
@@ -39,7 +35,9 @@ impl<T> SubTokenKind for MockTokenKind<T> {
   type TokenKind = Self;
   #[inline]
   fn kind_id() -> &'static TokenKindId<Self::TokenKind> {
-    cast_mock_token_kind_id()
+    // SAFETY: since all `TokenKindId<MockTokenKind<T>>` have the same memory layout,
+    // it should be safe to cast it to any `TokenKindId<MockTokenKind<T>>`.
+    unsafe { transmute(&Self::KIND_ID) }
   }
 }
 
