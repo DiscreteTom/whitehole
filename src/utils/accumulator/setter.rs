c3de1a9d2adb4a1816ef810058e246454cc9f81e
DiscreteTom <discrete_tom@outlook.com>
2024-08-13 08:56:52 +0800

    chore(utils): remove `AccumulatorSetter::to_mock/to_vec`

--- src/utils/accumulator/setter.rs
+++ src/utils/accumulator/setter.rs
@@ -21,32 +21,6 @@ impl<C> AccumulatorSetter<C> {
     (self.closure)(acc)
   }
 
-  /// Set the accumulator to `()` (discard all values).
-  #[inline]
-  pub fn to_mock<R>(self) -> R
-  where
-    C: FnOnce(()) -> R,
-  {
-    self.to(())
-  }
-
-  /// Accumulate values into a new vector.
-  /// # Performance
-  /// Creating a new vector each time is inefficient.
-  /// This should only be used for fast prototyping.
-  ///
-  /// In production if you want to collect values into a vector,
-  /// to prevent unnecessary allocations and de-allocations,
-  /// you should provision the capacity using [`Vec::with_capacity`]
-  /// and re-use the vector as much as possible with [`Self::to`].
-  #[inline]
-  pub fn to_vec<V, R>(self) -> R
-  where
-    C: FnOnce(Vec<V>) -> R,
-  {
-    self.to(vec![])
-  }
-
   /// Print the values to stdout (for debugging).
   ///
   /// Since [`DebugAccumulator`] is not a container,
