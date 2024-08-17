a385d7e6d1fdcb79858184d364785c09512c2437
DiscreteTom <discrete_tom@outlook.com>
2024-08-17 17:18:12 +0800

    chore(utils): add `OptionLookupTable::with_keys/map`, remove `data`

--- src/utils/lookup/option.rs
+++ src/utils/lookup/option.rs
@@ -15,6 +15,35 @@ impl<V> OptionLookupTable<V> {
     Self { data }
   }
 
+  /// Create a new instance with the given `keys`.
+  /// `keys` can be empty, unordered or duplicated.
+  /// Values are initialized with the provided `factory` if its key is present.
+  /// # Design
+  /// We use a key slice as the parameter, so the caller
+  /// doesn't need to deduplicate keys
+  /// (checking duplication in a lookup table is often more efficient).
+  pub fn with_keys(keys: &[usize], factory: impl Fn() -> V) -> Self {
+    // ensure the `keys` are not empty
+    if keys.is_empty() {
+      return Self::new(0);
+    }
+
+    // SAFETY: `keys` is not empty, so the `max` is safe to unwrap.
+    let max = *unsafe { keys.iter().max().unwrap_unchecked() };
+    let size = max + 1;
+    let mut res = Self::new(size);
+
+    for k in keys {
+      // SAFETY: `k` is guaranteed to be in the range of `0..=max`.
+      let d = unsafe { res.get_option_unchecked_mut(*k) };
+      if d.is_none() {
+        *d = Some(factory());
+      }
+    }
+
+    res
+  }
+
   /// Return the mutable reference to the value associated with the key.
   /// # Safety
   /// This method is unsafe because it doesn't check whether the key is out of range
@@ -24,13 +53,16 @@ impl<V> OptionLookupTable<V> {
     self.data.get_unchecked_mut(key)
   }
 
-  // TODO: remove this
-  #[inline]
-  pub fn data(&self) -> &[Option<V>] {
-    &self.data
+  pub fn map<R>(&self, mapper: impl Fn(&V) -> R) -> OptionLookupTable<R> {
+    OptionLookupTable {
+      data: self
+        .data
+        .iter()
+        .map(|v| v.as_ref().map(|v| mapper(v)))
+        .collect(),
+    }
   }
 
-  // TODO: remove this
   #[inline]
   pub fn data_mut(&mut self) -> &mut [Option<V>] {
     &mut self.data
