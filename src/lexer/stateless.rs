97d2081e3a7edf16dfc05ec406fbaaf1f8362d37
DiscreteTom <discrete_tom@outlook.com>
2024-09-11 20:42:56 +0800

    chore(lexer): optimize code

--- src/lexer/stateless.rs
+++ src/lexer/stateless.rs
@@ -99,7 +99,7 @@ mod utils;
 pub use options::*;
 
 use super::action::{Action, RcActionExec, RcActionProps};
-use crate::utils::lookup::{lookup::Lookup, option::OptionLookupTable};
+use crate::utils::lookup::option::OptionLookupTable;
 use head_map::HeadMap;
 use literal_map::LiteralMap;
 
@@ -188,9 +188,12 @@ impl<Kind, State, Heap> StatelessLexer<Kind, State, Heap> {
 #[cfg(test)]
 mod tests {
   use super::*;
-  use crate::lexer::{
-    action::{exact, regex},
-    token::{MockTokenKind, SubTokenKind},
+  use crate::{
+    lexer::{
+      action::{exact, regex},
+      token::{MockTokenKind, SubTokenKind},
+    },
+    utils::lookup::lookup::Lookup,
   };
   use head_map::RuntimeActions;
   use whitehole_macros::_token_kind;
