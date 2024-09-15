ed63cf5095b3da56a1ba7e0580a9ef401f47afaf
DiscreteTom <discrete_tom@outlook.com>
2024-09-16 01:09:12 +0800

    tests(kind): move unit tests

--- src/kind.rs
+++ src/kind.rs
@@ -213,3 +213,93 @@ pub use id::*;
 pub use mock::*;
 pub use sub::*;
 pub use whitehole_macros::whitehole_kind;
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use crate::kind::{DefaultSubKind, SubKind, SubKindId};
+  use whitehole_macros::_whitehole_kind;
+
+  #[_whitehole_kind]
+  #[derive(Debug, Clone, Default, PartialEq, Eq)]
+  pub enum MyKind {
+    #[default]
+    Unit,
+    Unnamed(i32),
+    Named {
+      name: i32,
+    },
+  }
+
+  #[test]
+  fn kind_macro() {
+    // generated structs
+    let _ = Unit;
+    let _ = Unnamed(42);
+    let _ = Named { name: 42 };
+
+    // unit variant is still unit variant instead of an unnamed variant
+    let _ = MyKind::Unit;
+
+    // other variants are transformed into unnamed variants
+    let _ = MyKind::Unnamed(Unnamed(42));
+    let _ = MyKind::Named(Named { name: 42 });
+
+    // sub token kinds into token kind
+    assert!(matches!(Unit.into(), MyKind::Unit));
+    assert!(matches!(Unnamed(42).into(), MyKind::Unnamed(Unnamed(42))));
+    assert!(matches!(
+      Named { name: 42 }.into(),
+      MyKind::Named(Named { name: 42 })
+    ));
+
+    // into token kind id binding
+    let b: KindIdBinding<MyKind> = Unit.into();
+    assert_eq!(b.id(), Unit::kind_id());
+    assert_eq!(b.take(), MyKind::Unit);
+    let b: KindIdBinding<MyKind> = Unnamed(42).into();
+    assert_eq!(b.id(), Unnamed::kind_id());
+    assert_eq!(b.take(), MyKind::Unnamed(Unnamed(42)));
+    let b: KindIdBinding<MyKind> = Named { name: 42 }.into();
+    assert_eq!(b.id(), Named::kind_id());
+    assert_eq!(b.take(), MyKind::Named(Named { name: 42 }));
+
+    // generated token kind id, as sub token kind.
+    let v: Vec<SubKindId<MyKind>> = vec![Unit::kind_id(), Unnamed::kind_id(), Named::kind_id()];
+    for (i, id) in v.iter().enumerate() {
+      for (j, id2) in v.iter().enumerate() {
+        if i == j {
+          assert_eq!(id, id2);
+        } else {
+          assert_ne!(id, id2);
+        }
+      }
+    }
+
+    // sub token kind into token kind id
+    assert_eq!(
+      <Unit as Into<SubKindId<MyKind>>>::into(Unit),
+      Unit::kind_id()
+    );
+    assert_eq!(
+      <Unnamed as Into<SubKindId<MyKind>>>::into(Unnamed(42)),
+      Unnamed::kind_id()
+    );
+    assert_eq!(
+      <Named as Into<SubKindId<MyKind>>>::into(Named { name: 42 }),
+      Named::kind_id()
+    );
+
+    // attributes are inherited by generated structs, e.g. Clone
+    let _ = Unit.clone();
+    let _ = Unnamed(42).clone();
+    let _ = Named { name: 42 }.clone();
+    let _ = MyKind::Unit.clone();
+    let _ = MyKind::Unnamed(Unnamed(42)).clone();
+    let _ = MyKind::Named(Named { name: 42 }).clone();
+
+    // default is working
+    assert!(matches!(MyKind::default(), MyKind::Unit));
+    assert_eq!(MyKind::default_kind_id(), Unit::kind_id());
+  }
+}
