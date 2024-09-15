2777a6b8a642fdb5c3156c1d4bb804eae263c36c
DiscreteTom <discrete_tom@outlook.com>
2024-09-16 01:05:13 +0800

    feat(kind): impl `DefaultSubKind` for `MockKind`

--- src/kind/mock.rs
+++ src/kind/mock.rs
@@ -1,6 +1,6 @@
-use super::SubKind;
+use super::{DefaultSubKind, SubKind};
 
-/// This implements [`SubKind`]
+/// This implements [`SubKind`] and [`DefaultSubKind`],
 /// and the [`SubKindId::value`](crate::kind::SubKindId::value)
 /// will always be `0`.
 /// This is useful as a placeholder or data carrier.
@@ -33,6 +33,10 @@ impl<T> SubKind for MockKind<T> {
   const VARIANT_INDEX: usize = 0;
 }
 
+impl<T> DefaultSubKind for MockKind<T> {
+  type Default = Self;
+}
+
 #[cfg(test)]
 mod tests {
   use super::*;
@@ -57,5 +61,16 @@ mod tests {
 
     assert_eq!(v1.id(), MockKind::kind_id());
     assert_eq!(v2.id(), MockKind::kind_id());
+  }
+
+  #[test]
+  fn default_mock_kind_id_binding() {
+    let v1: KindIdBinding<MockKind<i32>> = Default::default();
+    let v2: KindIdBinding<MockKind<bool>> = Default::default();
+
+    assert_eq!(v1.id(), MockKind::kind_id());
+    assert_eq!(v1.kind().data, 0);
+    assert_eq!(v2.id(), MockKind::kind_id());
+    assert!(!v2.kind().data);
   }
 }
