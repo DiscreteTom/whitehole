cfb2a245b3d1c059781f51ced7612fdb8ecbb994
DiscreteTom <discrete_tom@outlook.com>
2024-09-16 19:04:14 +0800

    chore(lexer): update comments for `Lexer`

--- src/lexer.rs
+++ src/lexer.rs
@@ -48,32 +48,21 @@ pub use token::*;
 
 use std::rc::Rc;
 
-/// This is the "stateful" lexer, it manages the [`Instant`] and the `State`.
-///
-/// The [`Instant`] is responsible to manage the text and the progress of the lexer.
-/// The `State` is provided by you and can be accessed by [`Action`](crate::lexer::action::Action)s
-/// to realize stateful lexing.
+/// This is the "stateful" lexer, it manages the [`Instant`],
+/// the [`State`](crate::lexer::action::ActionInput::state)
+/// and the [`Heap`](crate::lexer::action::ActionInput::heap).
 ///
 /// If you want a stateless experience, you can use [`StatelessLexer`].
 ///
-/// To create a lexer, you should use [`LexerBuilder`](crate::lexer::builder::LexerBuilder).
-/// # Design
-/// ## Why there is no `Lexer::errors` to store all the errors?
-/// Why the error accumulator is not a field of [`Lexer`]
-/// just like [`Lexer::state`],
-/// but a field of [`LexOptions`] which needs to be provided every time?
-///
-/// [`Lexer::state`] is just a value, but the error accumulator is a collection/container.
-/// We don't want unnecessary memory allocation, so we won't create the container
-/// for users. Users can create their own accumulator and manage its memory allocation.
-/// E.g. some users may just want to print the errors, so they don't need any container;
-/// some users may want to process errors after each lexing, and clear the container
-/// before next lexing to save memory; some users may want to store all errors
-/// in a container and process them later.
+/// To create a lexer, see [`LexerBuilder`](crate::lexer::builder::LexerBuilder).
 #[derive(Debug)]
 pub struct Lexer<'a, 'text, Kind, State = (), Heap = ()> {
+  /// See [`ActionInput::state`](crate::lexer::action::ActionInput::state).
+  ///
   /// You can mutate this directly if needed.
   pub state: State,
+  /// See [`ActionInput::heap`](crate::lexer::action::ActionInput::heap).
+  ///
   /// You can mutate this directly if needed.
   pub heap: Heap,
 
@@ -85,9 +74,9 @@ pub struct Lexer<'a, 'text, Kind, State = (), Heap = ()> {
 impl<'a, 'text, Kind, State: Clone, Heap: Clone> Clone for Lexer<'a, 'text, Kind, State, Heap> {
   /// Clone the lexer, including [`Self::state`] and [`Self::heap`].
   /// # Performance
-  /// Cloning the `Heap` might be expensive, you should use [`Lexer::snapshot`] to just clone the state,
+  /// Cloning the [`Self::heap`] might be expensive, you should use [`Lexer::snapshot`] to avoid cloning [`Self::heap`],
   /// and re-use one `heap` as much as possible.
-  /// If you want to prevent users from cloning [`Self::heap`], don't implement [`Clone`] for `Heap`.
+  /// If you want to prevent users from cloning this, don't implement [`Clone`] for `Heap`.
   #[inline]
   fn clone(&self) -> Self {
     Self {
@@ -100,9 +89,10 @@ impl<'a, 'text, Kind, State: Clone, Heap: Clone> Clone for Lexer<'a, 'text, Kind
 }
 
 impl<'a, 'text, Kind, State, Heap> Lexer<'a, 'text, Kind, State, Heap> {
-  /// Create a new lexer with the given stateless lexer, state and text.
-  /// For most cases you should use [`LexerBuilder`](crate::lexer::builder::LexerBuilder)
-  /// to create a lexer.
+  /// Create a new lexer with the given stateless lexer, state, heap and text.
+  ///
+  /// See [`LexerBuilder`](crate::lexer::builder::LexerBuilder) to create a lexer.
+  /// You can also use [`IntoLexer`] to convert some types to a lexer.
   #[inline]
   pub const fn new(
     stateless: Rc<StatelessLexer<'a, Kind, State, Heap>>,
@@ -123,15 +113,17 @@ impl<'a, 'text, Kind, State, Heap> Lexer<'a, 'text, Kind, State, Heap> {
   pub const fn stateless(&self) -> &Rc<StatelessLexer<'a, Kind, State, Heap>> {
     &self.stateless
   }
-  /// Get the lexer state.
-  /// You are not allowed to mutate the lexer state directly.
+  /// See [`Instant`].
+  /// You are not allowed to mutate this directly.
   #[inline]
   pub const fn instant(&self) -> &Instant<'text> {
     &self.instant
   }
 
   /// Consume self, return a new lexer with the same actions and a new text.
+  ///
   /// [`Self::instant`] and [`Self::state`] will be reset to default.
+  /// [`Self::heap`] won't change.
   #[inline]
   pub fn reload<'new_text>(self, text: &'new_text str) -> Lexer<'a, 'new_text, Kind, State, Heap>
   where
@@ -141,6 +133,7 @@ impl<'a, 'text, Kind, State, Heap> Lexer<'a, 'text, Kind, State, Heap> {
   }
 
   /// Consume self, return a new lexer with the same actions, a new text and the given state.
+  /// [`Self::heap`] won't change.
   #[inline]
   pub fn reload_with<'new_text>(
     self,
@@ -170,9 +163,10 @@ impl<'a, 'text, Kind, State, Heap> Lexer<'a, 'text, Kind, State, Heap> {
   }
 
   /// Peek the next token with the default options, without updating
-  /// [`Self::state`] and [`Self::state`].
+  /// [`Self::instant`] and [`Self::state`].
   ///
   /// [`Self::state`] will be cloned and returned.
+  /// [`Self::heap`] might be mutated.
   #[inline]
   pub fn peek(&mut self) -> (LexOutput<Token<Kind>, ()>, State)
   where
@@ -182,9 +176,10 @@ impl<'a, 'text, Kind, State, Heap> Lexer<'a, 'text, Kind, State, Heap> {
   }
 
   /// Peek the next token with custom options, without updating
-  /// [`Self::state`] and [`Self::state`].
+  /// [`Self::instant`] and [`Self::state`].
   ///
   /// [`Self::state`] will be cloned and returned.
+  /// [`Self::heap`] might be mutated.
   #[inline]
   pub fn peek_with<'literal, Fork: LexOptionsFork>(
     &mut self,
@@ -200,9 +195,10 @@ impl<'a, 'text, Kind, State, Heap> Lexer<'a, 'text, Kind, State, Heap> {
   }
 
   /// Peek the next token with custom options, without updating
-  /// [`Self::state`] and [`Self::state`].
+  /// [`Self::instant`] and [`Self::state`].
   ///
   /// [`Self::state`] will be cloned and returned.
+  /// [`Self::heap`] might be mutated.
   pub fn peek_with_options<'literal, Fork: LexOptionsFork>(
     &mut self,
     options: impl Into<LexOptions<'literal, Kind, Fork>>,
@@ -223,20 +219,20 @@ impl<'a, 'text, Kind, State, Heap> Lexer<'a, 'text, Kind, State, Heap> {
       },
     );
 
-    // don't update lexer state
+    // don't update state
 
     (output, new_state)
   }
 
   /// Try to yield the next token with the default options.
-  /// [`Self::state`] and [`Self::state`] will be updated.
+  /// [`Self::instant`] will be updated.
   #[inline]
   pub fn lex(&mut self) -> LexOutput<Token<Kind>, ()> {
     self.lex_with_options(LexOptions::new())
   }
 
   /// Try to yield the next token with custom options.
-  /// [`Self::state`] and [`Self::state`] will be updated.
+  /// [`Self::instant`] will be updated.
   #[inline]
   pub fn lex_with<'literal, Fork: LexOptionsFork>(
     &mut self,
@@ -246,7 +242,7 @@ impl<'a, 'text, Kind, State, Heap> Lexer<'a, 'text, Kind, State, Heap> {
   }
 
   /// Try to yield the next token with custom options.
-  /// [`Self::state`] and [`Self::state`] will be updated.
+  /// [`Self::instant`] will be updated.
   pub fn lex_with_options<'literal, Fork: LexOptionsFork>(
     &mut self,
     options: impl Into<LexOptions<'literal, Kind, Fork>>,
@@ -269,29 +265,25 @@ impl<'a, 'text, Kind, State, Heap> Lexer<'a, 'text, Kind, State, Heap> {
     output
   }
 
-  /// Digest the next `n` chars and optionally set [`Self::state`].
-  /// The caller should make sure `n` is smaller than the rest text length.
+  /// Lex with muted actions and the default options.
+  /// Returns [`None`] if the lexer is already trimmed.
+  /// [`Self::instant`] will be updated.
   #[inline]
-  pub fn digest_with(&mut self, n: usize, state: impl Into<Option<State>>) -> &mut Self {
-    self.instant.digest(n);
-    if let Some(state) = state.into() {
-      self.state = state;
-    }
-    self
-  }
-
-  /// Digest the next `n` chars and set [`Self::state`] to default.
-  /// The caller should make sure `n` is smaller than the rest text length.
-  #[inline]
-  pub fn digest(&mut self, n: usize) -> &mut Self
-  where
-    State: Default,
-  {
-    self.digest_with(n, State::default())
+  pub fn trim(&mut self) -> Option<TrimOutput> {
+    self.trim_with_options(TrimOptions)
   }
 
   /// Lex with muted actions and the provided options.
   /// Returns [`None`] if the lexer is already trimmed.
+  /// [`Self::instant`] will be updated.
+  #[inline]
+  pub fn trim_with(&mut self, f: impl FnOnce(TrimOptions) -> TrimOptions) -> Option<TrimOutput> {
+    self.trim_with_options(f(TrimOptions))
+  }
+
+  /// Lex with muted actions and the provided options.
+  /// Returns [`None`] if the lexer is already trimmed.
+  /// [`Self::instant`] will be updated.
   pub fn trim_with_options(&mut self, options: TrimOptions) -> Option<TrimOutput> {
     // return None if already trimmed
     if self.instant.trimmed() {
@@ -308,22 +300,34 @@ impl<'a, 'text, Kind, State, Heap> Lexer<'a, 'text, Kind, State, Heap> {
       },
     );
 
+    // update state
     self.instant.trim(res.digested);
 
     Some(res)
   }
 
-  /// Lex with muted actions and the provided options.
-  /// Returns [`None`] if the lexer is already trimmed.
+  /// Digest the next `n` chars and set [`Self::state`] to the default.
+  ///
+  /// The caller should make sure `n` is smaller than the rest text length,
+  /// this will be checked using [`debug_assert`].
   #[inline]
-  pub fn trim_with(&mut self, f: impl FnOnce(TrimOptions) -> TrimOptions) -> Option<TrimOutput> {
-    self.trim_with_options(f(TrimOptions))
+  pub fn digest(&mut self, n: usize) -> &mut Self
+  where
+    State: Default,
+  {
+    self.digest_with(n, State::default())
   }
 
-  /// Lex with muted actions and the default options.
-  /// Returns [`None`] if the lexer is already trimmed.
+  /// Digest the next `n` chars and optionally set [`Self::state`].
+  ///
+  /// The caller should make sure `n` is smaller than the rest text length,
+  /// this will be checked using [`debug_assert`].
   #[inline]
-  pub fn trim(&mut self) -> Option<TrimOutput> {
-    self.trim_with_options(TrimOptions)
+  pub fn digest_with(&mut self, n: usize, state: impl Into<Option<State>>) -> &mut Self {
+    self.instant.digest(n);
+    if let Some(state) = state.into() {
+      self.state = state;
+    }
+    self
   }
 }
