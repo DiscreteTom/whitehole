46244a3d45712767f63ba089364957171bfa6c6b
DiscreteTom <discrete_tom@outlook.com>
2024-11-09 15:35:39 +0800

    feat: add combinator::Output::map

--- src/combinator/output.rs
+++ src/combinator/output.rs
@@ -1,11 +1,21 @@
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Output<Kind> {
   pub kind: Kind,
-  /// How many bytes are digested by this action.
+  /// How many bytes are digested by this combinator.
   /// # Caveats
   /// `0` is allowed, but be careful with infinite loops.
   ///
   /// This value should be smaller than or equal to the length of
   /// [`Input::rest`](crate::combinator::input::Input::rest).
   pub digested: usize,
+}
+
+impl<Kind> Output<Kind> {
+  /// Convert [`Self::kind`] to a new kind.
+  pub fn map<NewKind>(self, f: impl FnOnce(Self) -> NewKind) -> Output<NewKind> {
+    Output {
+      digested: self.digested,
+      kind: f(self),
+    }
+  }
 }
