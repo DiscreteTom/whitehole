47304a1ab4d1e992ed6ab8ad6906b3a3a27548d2
DiscreteTom <discrete_tom@outlook.com>
2024-11-10 11:07:53 +0800

    refactor: remove BitAnd operator overloading for Combinator

--- src/combinator/operator.rs
+++ src/combinator/operator.rs
@@ -33,29 +33,6 @@ impl<'a, Kind: 'a, State: 'a, Heap: 'a, NewKind: 'a> ops::Add<Combinator<'a, New
   }
 }
 
-impl<'a, Kind: 'a, State: 'a, Heap: 'a, NewKind: 'a>
-  ops::BitAnd<Combinator<'a, NewKind, State, Heap>> for Combinator<'a, Kind, State, Heap>
-{
-  type Output = Combinator<'a, (Kind, NewKind), State, Heap>;
-
-  /// Parse with the left-hand side, then parse with the right-hand side.
-  ///
-  /// Return the output with the combined kind and the sum of the digested.
-  fn bitand(self, rhs: Combinator<'a, NewKind, State, Heap>) -> Self::Output {
-    Combinator::boxed(move |input| {
-      self.parse(input).and_then(|output| {
-        input
-          .digest(output.digested)
-          .and_then(|mut input| rhs.parse(&mut input))
-          .map(|rhs_output| Output {
-            kind: (output.kind, rhs_output.kind),
-            digested: output.digested + rhs_output.digested,
-          })
-      })
-    })
-  }
-}
-
 #[cfg(test)]
 mod tests {
   use super::*;
@@ -149,48 +126,6 @@ mod tests {
         .parse(&mut Input::new("123", 0, &mut (), &mut ()).unwrap()),
       Some(Output {
         kind: (),
-        digested: 2,
-      })
-    );
-  }
-
-  #[test]
-  fn combinator_bit_and() {
-    let rejecter = || Combinator::boxed(|_| Option::<Output<()>>::None);
-    let accepter_unit = || {
-      Combinator::boxed(|_| {
-        Some(Output {
-          kind: (),
-          digested: 1,
-        })
-      })
-    };
-    let accepter_int = || {
-      Combinator::boxed(|_| {
-        Some(Output {
-          kind: 123,
-          digested: 1,
-        })
-      })
-    };
-
-    // reject then accept, should return None
-    assert!((rejecter() & accepter_unit())
-      .parse(&mut Input::new("123", 0, &mut (), &mut ()).unwrap())
-      .is_none());
-
-    // accept then reject, should return None
-    assert!((accepter_unit() & rejecter())
-      .parse(&mut Input::new("123", 0, &mut (), &mut ()).unwrap())
-      .is_none());
-
-    // accept then accept, should return the sum of the digested
-    // with both kinds
-    assert_eq!(
-      (accepter_unit() & accepter_int())
-        .parse(&mut Input::new("123", 0, &mut (), &mut ()).unwrap()),
-      Some(Output {
-        kind: ((), 123),
         digested: 2,
       })
     );
