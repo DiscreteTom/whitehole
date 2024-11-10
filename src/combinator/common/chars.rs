83d92010d162182cf016fa044293be956c3be0f8
DiscreteTom <discrete_tom@outlook.com>
2024-11-10 14:14:16 +0800

    feat: add combinator::next

--- src/combinator/common/chars.rs
+++ src/combinator/common/chars.rs
@@ -1,6 +1,27 @@
 use super::eater_unchecked;
 use crate::combinator::Combinator;
 
+/// Match the next undigested char by the condition.
+/// Reject if the char is not matched.
+///
+/// # Examples
+/// ```
+/// # use whitehole::combinator::{Combinator, next};
+/// // match one ascii digit
+/// let _: Combinator<_> = next(|ch| ch.is_ascii_digit());
+/// ```
+pub fn next<'a, State, Heap>(
+  condition: impl Fn(char) -> bool + 'a,
+) -> Combinator<'a, (), State, Heap> {
+  eater_unchecked(move |input| {
+    let next = input.next();
+    if !condition(next) {
+      return 0;
+    }
+    next.len_utf8()
+  })
+}
+
 /// Match chars by the condition greedily.
 /// Reject if no char is matched.
 ///
@@ -29,6 +50,21 @@ pub fn chars<'a, State, Heap>(
 mod tests {
   use super::*;
   use crate::combinator::Input;
+
+  #[test]
+  fn combinator_next() {
+    // normal
+    assert_eq!(
+      next(|c| c.is_ascii_digit())
+        .parse(&mut Input::new("123", 0, &mut (), &mut ()).unwrap())
+        .map(|output| output.digested),
+      Some(1)
+    );
+    // reject
+    assert!(next(|c| c.is_ascii_alphabetic())
+      .parse(&mut Input::new("123", 0, &mut (), &mut ()).unwrap())
+      .is_none());
+  }
 
   #[test]
   fn combinator_chars() {
