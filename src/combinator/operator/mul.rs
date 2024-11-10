a601cd078a2404401f53ce48d287c8335eae203f
DiscreteTom <discrete_tom@outlook.com>
2024-11-10 22:22:10 +0800

    refactor: add `Fold` to make `Mul` generic

--- src/combinator/operator/mul.rs
+++ src/combinator/operator/mul.rs
@@ -3,14 +3,29 @@ use std::ops::{
   Bound, Mul, Range, RangeBounds, RangeFrom, RangeFull, RangeInclusive, RangeTo, RangeToInclusive,
 };
 
+/// A helper trait to fold kind values when calling [`Mul`] on [`Combinator`].
+///
+/// Built-in implementations are provided for `()`.
+pub trait Fold {
+  /// The fold result type.
+  type Output: Default;
+  /// Fold self with the current value.
+  fn fold(self, current: Self::Output) -> Self::Output;
+}
+
+impl Fold for () {
+  type Output = ();
+  fn fold(self, _: Self::Output) -> Self::Output {}
+}
+
 // TODO: Currently `usize` is not `RangeBounds<usize>` but in the future it might be.
 // At that time this mod could be simplified with
 // `impl Mul<RangeBounds> for Combinator`
 
-fn impl_mul_for_range_bound<'a, Kind: 'a, State: 'a, Heap: 'a>(
+fn impl_mul_for_range_bound<'a, Kind: Fold + 'a, State: 'a, Heap: 'a>(
   lhs: Combinator<'a, Kind, State, Heap>,
   rhs: impl RangeBounds<usize> + 'a,
-) -> Combinator<'a, Kind, State, Heap> {
+) -> Combinator<'a, Kind::Output, State, Heap> {
   Combinator::boxed(move |input| {
     // reject if repeat 0 times
     match rhs.end_bound() {
@@ -30,8 +45,9 @@ fn impl_mul_for_range_bound<'a, Kind: 'a, State: 'a, Heap: 'a>(
     // now the combinator is expected to repeat at least once
 
     let mut repeated = 0;
-    let output = lhs.parse(input).and_then(|mut output| {
+    let output = lhs.parse(input).and_then(|output| {
       repeated += 1;
+      let mut output = output.map(|kind| kind.fold(Kind::Output::default()));
       while match rhs.end_bound() {
         Bound::Included(&end) => repeated < end,
         Bound::Excluded(&end) => repeated + 1 < end,
@@ -43,7 +59,7 @@ fn impl_mul_for_range_bound<'a, Kind: 'a, State: 'a, Heap: 'a>(
         {
           Some(next_output) => {
             output.digested += next_output.digested;
-            output.kind = next_output.kind;
+            output.kind = next_output.kind.fold(output.kind);
             repeated += 1;
           }
           None => {
@@ -75,77 +91,83 @@ fn impl_mul_for_range_bound<'a, Kind: 'a, State: 'a, Heap: 'a>(
   })
 }
 
-impl<'a, Kind: 'a, State: 'a, Heap: 'a> Mul<usize> for Combinator<'a, Kind, State, Heap> {
-  type Output = Combinator<'a, Kind, State, Heap>;
+impl<'a, Kind: Fold + 'a, State: 'a, Heap: 'a> Mul<usize> for Combinator<'a, Kind, State, Heap> {
+  type Output = Combinator<'a, Kind::Output, State, Heap>;
 
   /// Repeat the combinator `rhs` times.
-  /// Return the output with the kind of the last output and the sum of the digested.
+  /// Return the output with the [`Fold`]-ed kind value and the sum of the digested.
   fn mul(self, rhs: usize) -> Self::Output {
     impl_mul_for_range_bound(self, rhs..=rhs)
   }
 }
 
-impl<'a, Kind: 'a, State: 'a, Heap: 'a> Mul<Range<usize>> for Combinator<'a, Kind, State, Heap> {
-  type Output = Combinator<'a, Kind, State, Heap>;
+impl<'a, Kind: Fold + 'a, State: 'a, Heap: 'a> Mul<Range<usize>>
+  for Combinator<'a, Kind, State, Heap>
+{
+  type Output = Combinator<'a, Kind::Output, State, Heap>;
 
   /// Repeat the combinator `rhs` times.
-  /// Return the output with the kind of the last output and the sum of the digested.
+  /// Return the output with the [`Fold`]-ed kind value and the sum of the digested.
   fn mul(self, rhs: Range<usize>) -> Self::Output {
     impl_mul_for_range_bound(self, rhs)
   }
 }
 
-impl<'a, Kind: 'a, State: 'a, Heap: 'a> Mul<RangeFrom<usize>>
+impl<'a, Kind: Fold + 'a, State: 'a, Heap: 'a> Mul<RangeFrom<usize>>
   for Combinator<'a, Kind, State, Heap>
 {
-  type Output = Combinator<'a, Kind, State, Heap>;
+  type Output = Combinator<'a, Kind::Output, State, Heap>;
 
   /// Repeat the combinator `rhs` times.
-  /// Return the output with the kind of the last output and the sum of the digested.
+  /// Return the output with the [`Fold`]-ed kind value and the sum of the digested.
   fn mul(self, rhs: RangeFrom<usize>) -> Self::Output {
     impl_mul_for_range_bound(self, rhs)
   }
 }
 
-impl<'a, Kind: 'a, State: 'a, Heap: 'a> Mul<RangeFull> for Combinator<'a, Kind, State, Heap> {
-  type Output = Combinator<'a, Kind, State, Heap>;
+impl<'a, Kind: Fold + 'a, State: 'a, Heap: 'a> Mul<RangeFull>
+  for Combinator<'a, Kind, State, Heap>
+{
+  type Output = Combinator<'a, Kind::Output, State, Heap>;
 
   /// Repeat the combinator `rhs` times.
-  /// Return the output with the kind of the last output and the sum of the digested.
+  /// Return the output with the [`Fold`]-ed kind value and the sum of the digested.
   fn mul(self, rhs: RangeFull) -> Self::Output {
     impl_mul_for_range_bound(self, rhs)
   }
 }
 
-impl<'a, Kind: 'a, State: 'a, Heap: 'a> Mul<RangeInclusive<usize>>
+impl<'a, Kind: Fold + 'a, State: 'a, Heap: 'a> Mul<RangeInclusive<usize>>
   for Combinator<'a, Kind, State, Heap>
 {
-  type Output = Combinator<'a, Kind, State, Heap>;
+  type Output = Combinator<'a, Kind::Output, State, Heap>;
 
   /// Repeat the combinator `rhs` times.
-  /// Return the output with the kind of the last output and the sum of the digested.
+  /// Return the output with the [`Fold`]-ed kind value and the sum of the digested.
   fn mul(self, rhs: RangeInclusive<usize>) -> Self::Output {
     impl_mul_for_range_bound(self, rhs)
   }
 }
 
-impl<'a, Kind: 'a, State: 'a, Heap: 'a> Mul<RangeTo<usize>> for Combinator<'a, Kind, State, Heap> {
-  type Output = Combinator<'a, Kind, State, Heap>;
+impl<'a, Kind: Fold + 'a, State: 'a, Heap: 'a> Mul<RangeTo<usize>>
+  for Combinator<'a, Kind, State, Heap>
+{
+  type Output = Combinator<'a, Kind::Output, State, Heap>;
 
   /// Repeat the combinator `rhs` times.
-  /// Return the output with the kind of the last output and the sum of the digested.
+  /// Return the output with the [`Fold`]-ed kind value and the sum of the digested.
   fn mul(self, rhs: RangeTo<usize>) -> Self::Output {
     impl_mul_for_range_bound(self, rhs)
   }
 }
 
-impl<'a, Kind: 'a, State: 'a, Heap: 'a> Mul<RangeToInclusive<usize>>
+impl<'a, Kind: Fold + 'a, State: 'a, Heap: 'a> Mul<RangeToInclusive<usize>>
   for Combinator<'a, Kind, State, Heap>
 {
-  type Output = Combinator<'a, Kind, State, Heap>;
+  type Output = Combinator<'a, Kind::Output, State, Heap>;
 
   /// Repeat the combinator `rhs` times.
-  /// Return the output with the kind of the last output and the sum of the digested.
+  /// Return the output with the [`Fold`]-ed kind value and the sum of the digested.
   fn mul(self, rhs: RangeToInclusive<usize>) -> Self::Output {
     impl_mul_for_range_bound(self, rhs)
   }
@@ -156,13 +178,22 @@ mod tests {
   use super::*;
   use crate::combinator::{Input, Output};
 
+  #[derive(Debug)]
+  struct MyKind(usize);
+  impl Fold for MyKind {
+    type Output = usize;
+    fn fold(self, current: Self::Output) -> Self::Output {
+      self.0 + current
+    }
+  }
+
   #[test]
   fn combinator_mul_usize() {
     let rejecter = || Combinator::boxed(|_| Option::<Output<()>>::None);
     let accepter = || {
       Combinator::boxed(|input| {
         Some(Output {
-          kind: input.next(),
+          kind: MyKind(input.start()),
           digested: 1,
         })
       })
@@ -179,11 +210,11 @@ mod tests {
       .parse(&mut Input::new("123", 0, &mut (), &mut ()).unwrap())
       .is_none());
 
-    // normal, apply the last output's kind and sum the digested
+    // normal, apply the folded kind value and sum the digested
     assert_eq!(
       (accepter() * 3).parse(&mut Input::new("123", 0, &mut (), &mut ()).unwrap()),
       Some(Output {
-        kind: '3',
+        kind: 3,
         digested: 3,
       })
     );
@@ -200,7 +231,7 @@ mod tests {
     let accepter = || {
       Combinator::boxed(|input| {
         Some(Output {
-          kind: input.next(),
+          kind: MyKind(input.start()),
           digested: 1,
         })
       })
@@ -216,11 +247,11 @@ mod tests {
       .parse(&mut Input::new("123", 0, &mut (), &mut ()).unwrap())
       .is_none());
 
-    // normal, apply the last output's kind and sum the digested
+    // normal, apply the folded kind value and sum the digested
     assert_eq!(
       (accepter() * (0..3)).parse(&mut Input::new("123", 0, &mut (), &mut ()).unwrap()),
       Some(Output {
-        kind: '2',
+        kind: 1,
         digested: 2,
       })
     );
@@ -237,7 +268,7 @@ mod tests {
     let accepter = || {
       Combinator::boxed(|input| {
         Some(Output {
-          kind: input.next(),
+          kind: MyKind(input.start()),
           digested: 1,
         })
       })
@@ -248,11 +279,11 @@ mod tests {
       .parse(&mut Input::new("123", 0, &mut (), &mut ()).unwrap())
       .is_none());
 
-    // normal, apply the last output's kind and sum the digested
+    // normal, apply the folded kind value and sum the digested
     assert_eq!(
       (accepter() * (0..)).parse(&mut Input::new("123", 0, &mut (), &mut ()).unwrap()),
       Some(Output {
-        kind: '3',
+        kind: 3,
         digested: 3,
       })
     );
@@ -269,7 +300,7 @@ mod tests {
     let accepter = || {
       Combinator::boxed(|input| {
         Some(Output {
-          kind: input.next(),
+          kind: MyKind(input.start()),
           digested: 1,
         })
       })
@@ -280,11 +311,11 @@ mod tests {
       .parse(&mut Input::new("123", 0, &mut (), &mut ()).unwrap())
       .is_none());
 
-    // normal, apply the last output's kind and sum the digested
+    // normal, apply the folded kind value and sum the digested
     assert_eq!(
       (accepter() * (..)).parse(&mut Input::new("123", 0, &mut (), &mut ()).unwrap()),
       Some(Output {
-        kind: '3',
+        kind: 3,
         digested: 3,
       })
     );
@@ -296,7 +327,7 @@ mod tests {
     let accepter = || {
       Combinator::boxed(|input| {
         Some(Output {
-          kind: input.next(),
+          kind: MyKind(input.start()),
           digested: 1,
         })
       })
@@ -312,11 +343,11 @@ mod tests {
       .parse(&mut Input::new("123", 0, &mut (), &mut ()).unwrap())
       .is_none());
 
-    // normal, apply the last output's kind and sum the digested
+    // normal, apply the folded kind value and sum the digested
     assert_eq!(
       (accepter() * (0..=3)).parse(&mut Input::new("123", 0, &mut (), &mut ()).unwrap()),
       Some(Output {
-        kind: '3',
+        kind: 3,
         digested: 3,
       })
     );
@@ -333,7 +364,7 @@ mod tests {
     let accepter = || {
       Combinator::boxed(|input| {
         Some(Output {
-          kind: input.next(),
+          kind: MyKind(input.start()),
           digested: 1,
         })
       })
@@ -349,11 +380,11 @@ mod tests {
       .parse(&mut Input::new("123", 0, &mut (), &mut ()).unwrap())
       .is_none());
 
-    // normal, apply the last output's kind and sum the digested
+    // normal, apply the folded kind value and sum the digested
     assert_eq!(
       (accepter() * (..3)).parse(&mut Input::new("123", 0, &mut (), &mut ()).unwrap()),
       Some(Output {
-        kind: '2',
+        kind: 1,
         digested: 2,
       })
     );
@@ -365,7 +396,7 @@ mod tests {
     let accepter = || {
       Combinator::boxed(|input| {
         Some(Output {
-          kind: input.next(),
+          kind: MyKind(input.start()),
           digested: 1,
         })
       })
@@ -381,11 +412,11 @@ mod tests {
       .parse(&mut Input::new("123", 0, &mut (), &mut ()).unwrap())
       .is_none());
 
-    // normal, apply the last output's kind and sum the digested
+    // normal, apply the folded kind value and sum the digested
     assert_eq!(
       (accepter() * (..=3)).parse(&mut Input::new("123", 0, &mut (), &mut ()).unwrap()),
       Some(Output {
-        kind: '3',
+        kind: 3,
         digested: 3,
       })
     );
