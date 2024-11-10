5fc5325d7df6b5e1d5da8e75f919eeb7aabcddc6
DiscreteTom <discrete_tom@outlook.com>
2024-11-10 23:16:57 +0800

    docs: add doc for combinator mod

--- src/combinator.rs
+++ src/combinator.rs
@@ -1,4 +1,72 @@
-//! Building block of a parser.
+//! The building block of a lexer or a parser.
+//! # Basic Usage
+//! ## Provided Combinators
+//! To get started, you can use the provided combinators like [`exact`],
+//! which will match a string or a char exactly:
+//! ```
+//! # use whitehole::combinator::{Combinator, exact};
+//! let _: Combinator<_> = exact("true");
+//! ```
+//! See functions in this module for more provided combinators.
+//! ## Combine
+//! You can use operators to combine multiple combinators
+//! to digest more complex content:
+//! ```
+//! # use whitehole::combinator::{Combinator, exact};
+//! // match "true" then match "false"
+//! let _: Combinator<_> = exact("true") + exact("false");
+//!
+//! // match "true" or "false"
+//! let _: Combinator<_> = exact("true") | exact("false");
+//!
+//! // you can use a string or a char as a shortcut for `exact`
+//! let _: Combinator<_> = exact("true") + "false";
+//! let _: Combinator<_> = exact("true") | "false";
+//!
+//! // you can use an usize number as a shortcut for `eat`
+//! // which will eat the next n bytes
+//! let _: Combinator<_> = exact("true") + 1;
+//! let _: Combinator<_> = exact("true") | 1;
+//! ```
+//! ## Repeat
+//! To repeat a combinator, just use the `*` operator:
+//! ```
+//! # use whitehole::combinator::{Combinator, exact};
+//! // repeat the combinator 2 times
+//! let _: Combinator<_> = exact("true") * 2;
+//! // equals to
+//! let _: Combinator<_> = exact("true") + "true";
+//!
+//! // repeat the combinator with a range, greedy
+//! let _: Combinator<_> = exact("true") * (1..=3);
+//! // similar to but faster than
+//! let _: Combinator<_> =
+//!     (exact("true") + "true" + "true")
+//!   | (exact("true") + "true")
+//!   |  exact("true");
+//!
+//! // allowing repeat for 0 or more times
+//! // so that even if the combinator is rejected,
+//! // the whole combinator will still be accepted with 0 bytes digested
+//! let _: Combinator<_> = exact("true") * (..);
+//! let _: Combinator<_> = exact("true") * (..=3);
+//!
+//! // repeating for at most 0 times will
+//! // always accept 0 bytes without executing the combinator.
+//! // you shouldn't use this for most cases
+//! let _: Combinator<_> = exact("true") * 0;
+//! let _: Combinator<_> = exact("true") * (..1);
+//! let _: Combinator<_> = exact("true") * (..=0);
+//! ```
+//! ## Decorator
+//! You can use combinator decorators to modify the behavior of a combinator.
+//! Unlike combining combinators, decorators won't change the digested content:
+//! ```
+//! # use whitehole::combinator::{Combinator, exact};
+//! // make the combinator optional
+//! let _: Combinator<_> = exact("true").optional();
+//! ```
+//! See [`Combinator`]'s methods for more decorators.
 
 mod common;
 mod decorator;
@@ -16,7 +84,7 @@ pub use output::*;
 pub type CombinatorExec<'a, Kind, State = (), Heap = ()> =
   Box<dyn Fn(&mut Input<&mut State, &mut Heap>) -> Option<Output<Kind>> + 'a>;
 
-/// Building block of a parser.
+/// See the [module-level documentation](crate::combinator).
 pub struct Combinator<'a, Kind, State = (), Heap = ()> {
   exec: CombinatorExec<'a, Kind, State, Heap>,
 }
