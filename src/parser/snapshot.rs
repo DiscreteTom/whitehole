a26a3711e9968e5721d11d945b5d31976ad8a5bd
DiscreteTom <discrete_tom@outlook.com>
2024-11-17 22:06:32 +0800

    refactor: update `parser::Snapshot` to apply `Instant`

--- src/parser/snapshot.rs
+++ src/parser/snapshot.rs
@@ -1,10 +1,12 @@
+use super::Instant;
+
 /// The snapshot of [`Parser`](crate::parser::Parser).
 ///
 /// This can be created by [`Parser::snapshot`](crate::parser::Parser::snapshot)
 /// and used by [`Parser::restore`](crate::parser::Parser::restore).
 ///
 /// You can't construct this manually because
-/// you shouldn't modify [`Self::digested`] and [`Self::text`] directly.
+/// you shouldn't modify [`Self::instant`] directly.
 ///
 /// Since `State` should be cheap to clone,
 /// this is also cheap to create or clone.
@@ -13,23 +15,16 @@ pub struct Snapshot<'text, State> {
   /// See [`Parser::state`](crate::parser::Parser::state).
   /// You can modify this if needed.
   pub state: State,
-  /// See [`Parser::text`](crate::parser::Parser::text).
-  pub(super) text: &'text str,
-  /// See [`Parser::digested`](crate::parser::Parser::digested).
-  pub(super) digested: usize,
+  /// See [`Self::instant`].
+  pub(super) instant: Instant<'text>,
 }
 
 impl<'text, State> Snapshot<'text, State> {
-  /// See [`Parser::text`](crate::parser::Parser::text).
+  /// See [`Parser::instant`](crate::parser::Parser::instant).
   /// You can't modify this manually.
-  pub const fn text(&self) -> &'text str {
-    self.text
-  }
-
-  /// See [`Parser::digested`](crate::parser::Parser::digested).
-  /// You can't modify this manually.
-  pub const fn digested(&self) -> usize {
-    self.digested
+  #[inline]
+  pub const fn instant(&self) -> &Instant<'text> {
+    &self.instant
   }
 }
 
@@ -42,20 +37,10 @@ mod tests {
     assert_eq!(
       Snapshot {
         state: (),
-        text: "123",
-        digested: 1
+        instant: Instant::new("")
       }
-      .text(),
-      "123"
-    );
-    assert_eq!(
-      Snapshot {
-        state: (),
-        text: "123",
-        digested: 1
-      }
-      .digested(),
-      1
+      .instant(),
+      &Instant::new("")
     );
   }
 }
