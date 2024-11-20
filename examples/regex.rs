e9b8c46b8c97934b7ba4f0f5bcf74bf9db8bca95
DiscreteTom <discrete_tom@outlook.com>
2024-11-20 22:29:33 +0800

    chore: add unit tests for examples/regex

--- examples/regex.rs
+++ examples/regex.rs
@@ -1,22 +1,34 @@
 use regex::Regex;
-use whitehole::{
-  combinator::eater_unchecked,
-  parse::{Input, Parse},
-  Combinator,
-};
+use whitehole::{combinator::eater_unchecked, Combinator};
 
 /// Create a combinator from a regex.
-fn regex<State, Heap>(s: &str) -> Combinator!((), State, Heap) {
+pub fn regex<State, Heap>(s: &str) -> Combinator!((), State, Heap) {
   let re = Regex::new(s).unwrap();
   unsafe { eater_unchecked(move |input| re.find(input.rest()).map(|m| m.len()).unwrap_or(0)) }
 }
 
-fn main() {
-  assert_eq!(
-    regex(r"\d+")
-      .parse(&mut Input::new("123", 0, &mut (), &mut ()).unwrap())
-      .unwrap()
-      .rest,
-    ""
-  );
+fn main() {}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use whitehole::parse::{Input, Parse};
+
+  #[test]
+  fn test_regex() {
+    assert_eq!(
+      regex(r"\d+")
+        .parse(&mut Input::new("123", 0, &mut (), &mut ()).unwrap())
+        .unwrap()
+        .rest,
+      ""
+    );
+    assert_eq!(
+      regex(r"\d+")
+        .parse(&mut Input::new("123abc", 0, &mut (), &mut ()).unwrap())
+        .unwrap()
+        .rest,
+      "abc"
+    );
+  }
 }
