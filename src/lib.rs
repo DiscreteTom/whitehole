6adaaa2bf2c75f45a9c14f33e7bd09c78fc2a95a
DiscreteTom <discrete_tom@outlook.com>
2024-11-20 22:44:53 +0800

    docs: update crate doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,18 @@
-//! ## Getting Started
+//! A parser combinator framework.
+//!
+//! # Getting Started
 //!
 //! Here is the recommended order of learning this project:
 //!
 //! - [`parse`] (optional)
 //! - [`combinator`]
 //! - [`parser`]
+//!
+//! # Related
+//!
+//! - [`in_str`](https://github.com/DiscreteTom/in_str/):
+//!   A procedural macro to generate a closure that checks
+//!   if a character is in the provided literal string.
 
 pub mod combinator;
 pub mod node;
