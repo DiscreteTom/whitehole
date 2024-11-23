6ef0213db92daf806acdb3a691da3d3bbb9a1309
DiscreteTom <discrete_tom@outlook.com>
2024-11-23 17:49:25 +0800

    chore: update examples/json_parser family

--- examples/json_parser_with_inter_mut.rs
+++ examples/json_parser_with_inter_mut.rs
@@ -1,16 +1,17 @@
 use in_str::in_str;
 use std::{cell::OnceCell, rc::Rc};
 use whitehole::{
-  combinator::{eat, next, wrap, Combinator},
+  combinator::{eat, next, wrap},
   parse::Parse,
   parser::{Builder, Parser},
 };
 
-fn build_parser(s: &str) -> Parser<impl Parse> {
+pub fn build_parser_with_inter_mut(s: &str) -> Parser<impl Parse> {
   Builder::new()
     .entry(|b| {
-      // use `b.next` instead of `next` for better type inference.
-      // use a closure to generate combinator for better performance (via inlining), instead of wrapping the combinator in an Rc.
+      // Use `b.next` instead of `next` for better type inference. This is optional.
+      // To re-use a combinator for multiple times, instead of wrapping the combinator in an Rc,
+      // use a closure to generate the combinator for better runtime performance (via inlining).
       let ws = || b.next(in_str!(" \t\r\n")) * (1..);
       let number = || {
         let digit_1_to_9 = next(|c| matches!(c, '1'..='9'));
@@ -29,57 +30,37 @@ fn build_parser(s: &str) -> Parser<impl Parse> {
         eat('"') + body.optional() + '"'
       };
 
-      // for combinators that will be used recursively, we have to use a pointer with interior mutability.
-      // here we use Rc<OnceCell<Combinator<_>>> to store the combinator.
-      macro_rules! init_rc {
-        ($name:ident, $rc_name:ident) => {
-          let $rc_name: Rc<OnceCell<Combinator<_>>> = Rc::new(OnceCell::new());
-          let $name = || {
-            let $rc_name = $rc_name.clone();
-            wrap(move |input| unsafe { $rc_name.get().unwrap_unchecked() }.parse(input))
-          };
-        };
-      }
-      init_rc!(array, array_rc);
-      init_rc!(object_item, object_item_rc);
-      init_rc!(object, object_rc);
-
-      // the last recursive combinator doesn't need interior mutability
-      // because we can write the closure with previous combinators.
-      // use `dyn` to prevent recursive type
-      let value_rc: Rc<dyn Parse<Kind = ()>> = Rc::new(wrap({
-        let parser = array() | object() | number() | string() | "true" | "false" | "null";
-        move |input| parser.parse(input)
-      }));
+      // `value` will indirectly recurse to itself, so we need special treatment.
+      // Use `Rc` to make it clone-able, use `OnceCell` to initialize it later,
+      // use `Box<dyn>` to prevent recursive/infinite type.
+      let value_rc: Rc<OnceCell<Box<dyn Parse<Kind = ()>>>> = Rc::new(OnceCell::new());
       let value = || {
         let value_rc = value_rc.clone();
-        wrap(move |input| value_rc.parse(input))
+        // SAFETY: we will initialize `value_rc` later before calling this closure.
+        wrap(move |input| unsafe { value_rc.get().unwrap_unchecked() }.parse(input))
       };
 
-      // set rc combinator value
-      array_rc
-        .set(wrap({
-          let parser = eat('[')
-            + ws().optional()
-            + ((value() + ws().optional()) * (.., eat(',') + ws().optional())).optional()
-            + ']';
+      // Now we can use `value` in `array` and `object`.
+      let array = || {
+        eat('[')
+          + ws().optional()
+          + ((value() + ws().optional()) * (.., eat(',') + ws().optional())).optional()
+          + ']'
+      };
+      let object = || {
+        let object_item = string() + ws().optional() + eat(':') + ws().optional() + value();
+        eat('{')
+          + ws().optional()
+          + ((object_item + ws().optional()) * (.., eat(',') + ws().optional())).optional()
+          + '}'
+      };
+
+      // Finally, init `value` with `array` and `object`.
+      value_rc
+        .set(Box::new(wrap({
+          let parser = array() | object() | number() | string() | "true" | "false" | "null";
           move |input| parser.parse(input)
-        }))
-        .ok();
-      object_item_rc
-        .set(wrap({
-          let parser = string() + ws().optional() + eat(':') + ws().optional() + value();
-          move |input| parser.parse(input)
-        }))
-        .ok();
-      object_rc
-        .set(wrap({
-          let parser = eat('{')
-            + ws().optional()
-            + ((object_item() + ws().optional()) * (.., eat(',') + ws().optional())).optional()
-            + '}';
-          move |input| parser.parse(input)
-        }))
+        })))
         .ok();
 
       ws() | value()
@@ -87,41 +68,49 @@ fn build_parser(s: &str) -> Parser<impl Parse> {
     .build(s)
 }
 
-fn main() {
-  let s = r#"
-    {
-      "name": "John Doe",
-      "age": 30,
-      "is_student": false,
-      "scores": [100, 90, 80],
-      "address": {
-        "city": "New York",
-        "zip": "10001"
+fn main() {}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_json_parser() {
+    let s = r#"
+      {
+        "name": "John Doe",
+        "age": 30,
+        "is_student": false,
+        "scores": [100, 90, 80],
+        "address": {
+          "city": "New York",
+          "zip": "10001"
+        }
+      }
+    "#;
+
+    let mut parser = build_parser_with_inter_mut(s);
+
+    loop {
+      let output = parser.parse();
+      if let Some(node) = output {
+        println!(
+          "{}..{}: {:?}",
+          node.range.start,
+          node.range.end,
+          &s[node.range.clone()]
+        );
+      } else {
+        break;
       }
     }
-  "#;
 
-  let mut parser = build_parser(s);
-
-  loop {
-    let output = parser.parse();
-    if let Some(node) = output {
-      println!(
-        "{}..{}: {}",
-        node.range.start,
-        node.range.end,
-        &s[node.range.clone()]
+    let rest = parser.instant().rest();
+    if !rest.is_empty() {
+      panic!(
+        "lexer failed to consume the whole input, remaining: {:?}",
+        &rest[..100.min(rest.len())]
       );
-    } else {
-      break;
     }
-  }
-
-  let rest = parser.instant().rest();
-  if !rest.is_empty() {
-    panic!(
-      "lexer failed to consume the whole input, remaining: {}",
-      &rest[..100.min(rest.len())]
-    );
   }
 }
--- examples/json_parser_with_static.rs
+++ examples/json_parser_with_static.rs
@@ -7,104 +7,102 @@ use whitehole::{
   Combinator,
 };
 
-fn ws() -> Combinator!() {
-  next(in_str!(" \t\r\n")) * (1..)
-}
+pub fn build_parser_with_static(s: &str) -> Parser<impl Parse> {
+  // To re-use a combinator for multiple times, instead of wrapping the combinator in an Rc,
+  // use a function to generate the combinator for better runtime performance (via inlining).
+  fn ws() -> Combinator!() {
+    next(in_str!(" \t\r\n")) * (1..)
+  }
 
-fn number() -> Combinator!() {
-  let digit_1_to_9 = next(|c| matches!(c, '1'..='9'));
-  let digits = || next(|c| c.is_ascii_digit()) * (1..);
-  let integer = eat('0') | (digit_1_to_9 + digits().optional());
-  let fraction = eat('.') + digits();
-  let exponent = (eat('e') | 'E') + (eat('-') | '+').optional() + digits();
-  eat('-').optional() + integer + fraction.optional() + exponent.optional()
-}
+  fn number() -> Combinator!() {
+    let digit_1_to_9 = next(|c| matches!(c, '1'..='9'));
+    let digits = || next(|c| c.is_ascii_digit()) * (1..);
+    let integer = eat('0') | (digit_1_to_9 + digits().optional());
+    let fraction = eat('.') + digits();
+    let exponent = (eat('e') | 'E') + (eat('-') | '+').optional() + digits();
+    eat('-').optional() + integer + fraction.optional() + exponent.optional()
+  }
 
-fn string() -> Combinator!() {
-  let escape =
-    eat('\\') + (next(in_str!("\"\\/bfnrt")) | (eat('u') + next(|c| c.is_ascii_hexdigit()) * 4));
-  let non_escape =
-    next(|c| c != '"' && c != '\\' && matches!(c, '\u{0020}'..='\u{10ffff}')) * (1..);
-  let body = (escape | non_escape) * ..;
-  eat('"') + body.optional() + '"'
-}
+  fn string() -> Combinator!() {
+    let escape =
+      eat('\\') + (next(in_str!("\"\\/bfnrt")) | (eat('u') + next(|c| c.is_ascii_hexdigit()) * 4));
+    let non_escape =
+      next(|c| c != '"' && c != '\\' && matches!(c, '\u{0020}'..='\u{10ffff}')) * (1..);
+    let body = (escape | non_escape) * ..;
+    eat('"') + body.optional() + '"'
+  }
 
-fn value() -> Combinator!() {
-  static VALUE: LazyLock<Box<dyn Parse<Kind = ()> + Send + Sync>> = LazyLock::new(|| {
-    Box::new(array() | object() | number() | string() | "true" | "false" | "null")
-  });
-  wrap(|input| VALUE.parse(input))
-}
+  fn array() -> Combinator!() {
+    eat('[')
+      + ws().optional()
+      + ((value() + ws().optional()) * (.., eat(',') + ws().optional())).optional()
+      + ']'
+  }
 
-fn array() -> Combinator!() {
-  static ARRAY: LazyLock<Box<dyn Parse<Kind = ()> + Send + Sync>> = LazyLock::new(|| {
-    Box::new(
-      eat('[')
-        + ws().optional()
-        + ((value() + ws().optional()) * (.., eat(',') + ws().optional())).optional()
-        + ']',
-    )
-  });
-  wrap(|input| ARRAY.parse(input))
-}
+  fn object() -> Combinator!() {
+    let object_item = string() + ws().optional() + eat(':') + ws().optional() + value();
+    eat('{')
+      + ws().optional()
+      + ((object_item + ws().optional()) * (.., eat(',') + ws().optional())).optional()
+      + '}'
+  }
 
-fn object_item() -> Combinator!() {
-  static OBJECT_ITEM: LazyLock<Box<dyn Parse<Kind = ()> + Send + Sync>> =
-    LazyLock::new(|| Box::new(string() + ws().optional() + eat(':') + ws().optional() + value()));
-  wrap(|input| OBJECT_ITEM.parse(input))
-}
+  // `value` will indirectly recurse to itself, so we need special treatment.
+  // Use `LazyLock` to create a static `Parse` implementor,
+  // use `Box<dyn>` to prevent recursive/infinite type.
+  fn value() -> Combinator!() {
+    static VALUE: LazyLock<Box<dyn Parse<Kind = ()> + Send + Sync>> = LazyLock::new(|| {
+      Box::new(array() | object() | number() | string() | "true" | "false" | "null")
+    });
+    wrap(|input| VALUE.parse(input))
+  }
 
-fn object() -> Combinator!() {
-  static OBJECT: LazyLock<Box<dyn Parse<Kind = ()> + Send + Sync>> = LazyLock::new(|| {
-    Box::new(
-      eat('{')
-        + ws().optional()
-        + ((object_item() + ws().optional()) * (.., eat(',') + ws().optional())).optional()
-        + '}',
-    )
-  });
-  wrap(|input| OBJECT.parse(input))
-}
-
-fn build_parser(s: &str) -> Parser<impl Parse> {
   Builder::new().entry(|_| ws() | value()).build(s)
 }
 
-fn main() {
-  let s = r#"
-    {
-      "name": "John Doe",
-      "age": 30,
-      "is_student": false,
-      "scores": [100, 90, 80],
-      "address": {
-        "city": "New York",
-        "zip": "10001"
+fn main() {}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_json_parser() {
+    let s = r#"
+      {
+        "name": "John Doe",
+        "age": 30,
+        "is_student": false,
+        "scores": [100, 90, 80],
+        "address": {
+          "city": "New York",
+          "zip": "10001"
+        }
+      }
+    "#;
+
+    let mut parser = build_parser_with_static(s);
+
+    loop {
+      let output = parser.parse();
+      if let Some(node) = output {
+        println!(
+          "{}..{}: {:?}",
+          node.range.start,
+          node.range.end,
+          &s[node.range.clone()]
+        );
+      } else {
+        break;
       }
     }
-  "#;
 
-  let mut parser = build_parser(s);
-
-  loop {
-    let output = parser.parse();
-    if let Some(node) = output {
-      println!(
-        "{}..{}: {}",
-        node.range.start,
-        node.range.end,
-        &s[node.range.clone()]
+    let rest = parser.instant().rest();
+    if !rest.is_empty() {
+      panic!(
+        "lexer failed to consume the whole input, remaining: {:?}",
+        &rest[..100.min(rest.len())]
       );
-    } else {
-      break;
     }
-  }
-
-  let rest = parser.instant().rest();
-  if !rest.is_empty() {
-    panic!(
-      "lexer failed to consume the whole input, remaining: {}",
-      &rest[..100.min(rest.len())]
-    );
   }
 }
