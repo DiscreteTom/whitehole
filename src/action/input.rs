7024b9483070d8fd16af7421a51896122ef92867
DiscreteTom <discrete_tom@outlook.com>
2024-12-24 14:20:26 +0000

    chore: move `Input::digest` and `digest_unchecked` to `output.rs`

--- src/action/input.rs
+++ src/action/input.rs
@@ -1,5 +1,3 @@
-use super::Output;
-
 /// The input of [`Action::exec`](crate::action::Action::exec).
 ///
 /// Once created, only [`Self::state`] and [`Self::heap`] can be mutated.
@@ -103,29 +101,6 @@ impl<'text, StateRef, HeapRef> Input<'text, StateRef, HeapRef> {
     // SAFETY: `self.rest()` is guaranteed to be not empty.
     unsafe { self.rest().chars().next().unwrap_unchecked() }
   }
-
-  /// Try to build an [`Output`] by digesting `n` bytes.
-  /// Return [`None`] if the [`Output::rest`] can't be built
-  /// as a valid UTF-8 string.
-  #[inline]
-  pub fn digest(&self, n: usize) -> Option<Output<'text, ()>> {
-    self.rest.get(n..).map(|rest| Output { value: (), rest })
-  }
-
-  /// Try to build an [`Output`] by digesting `n` bytes.
-  /// # Safety
-  /// You should ensure that [`Output::rest`] can be built
-  /// as a valid UTF-8 string.
-  /// This will be checked using [`debug_assert!`].
-  /// For the checked version, see [`Self::digest`].
-  #[inline]
-  pub unsafe fn digest_unchecked(&self, n: usize) -> Output<'text, ()> {
-    debug_assert!(self.rest.get(n..).is_some());
-    Output {
-      value: (),
-      rest: self.rest.get_unchecked(n..),
-    }
-  }
 }
 
 impl<'text, State, Heap> Input<'text, &mut State, &mut Heap> {
@@ -182,46 +157,5 @@ mod tests {
     assert_eq!(input.reload("23").unwrap().start(), 1);
     assert_eq!(input.reload("3").unwrap().start(), 2);
     assert!(input.reload("").is_none());
-  }
-
-  #[test]
-  fn input_digest() {
-    let mut state = ();
-    let mut heap = ();
-    let input = Input::new("123", 0, &mut state, &mut heap).unwrap();
-    assert_eq!(input.digest(3).map(|output| output.rest), Some(""));
-    assert_eq!(input.digest(2).map(|output| output.rest), Some("3"));
-    assert_eq!(input.digest(1).map(|output| output.rest), Some("23"));
-    assert_eq!(input.digest(0).map(|output| output.rest), Some("123"));
-    assert!(input.digest(4).is_none());
-  }
-
-  #[test]
-  fn input_digest_unchecked() {
-    let mut state = ();
-    let mut heap = ();
-    let input = Input::new("123", 0, &mut state, &mut heap).unwrap();
-    assert_eq!(unsafe { input.digest_unchecked(3).rest }, "");
-    assert_eq!(unsafe { input.digest_unchecked(2).rest }, "3");
-    assert_eq!(unsafe { input.digest_unchecked(1).rest }, "23");
-    assert_eq!(unsafe { input.digest_unchecked(0).rest }, "123");
-  }
-
-  #[test]
-  #[should_panic]
-  fn input_digest_unchecked_overflow() {
-    let mut state = ();
-    let mut heap = ();
-    let input = Input::new("123", 0, &mut state, &mut heap).unwrap();
-    unsafe { input.digest_unchecked(4) };
-  }
-
-  #[test]
-  #[should_panic]
-  fn input_digest_unchecked_invalid_code_point() {
-    let mut state = ();
-    let mut heap = ();
-    let input = Input::new("好", 0, &mut state, &mut heap).unwrap();
-    unsafe { input.digest_unchecked(1) };
   }
 }
--- src/action/output.rs
+++ src/action/output.rs
@@ -1,11 +1,38 @@
+use super::Input;
+
 /// The output of [`Action::exec`](crate::action::Action::exec).
-/// Usually built by [`Input::digest`](crate::action::Input::digest).
+/// Usually built by [`Input::digest`].
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Output<'text, Value> {
   /// The yielded value.
   pub value: Value,
   /// The rest of the input text.
   pub rest: &'text str,
+}
+
+impl<'text, StateRef, HeapRef> Input<'text, StateRef, HeapRef> {
+  /// Try to build an [`Output`] by digesting `n` bytes.
+  /// Return [`None`] if the [`Output::rest`] can't be built
+  /// as a valid UTF-8 string.
+  #[inline]
+  pub fn digest(&self, n: usize) -> Option<Output<'text, ()>> {
+    self.rest().get(n..).map(|rest| Output { value: (), rest })
+  }
+
+  /// Try to build an [`Output`] by digesting `n` bytes.
+  /// # Safety
+  /// You should ensure that [`Output::rest`] can be built
+  /// as a valid UTF-8 string.
+  /// This will be checked using [`debug_assert!`].
+  /// For the checked version, see [`Self::digest`].
+  #[inline]
+  pub unsafe fn digest_unchecked(&self, n: usize) -> Output<'text, ()> {
+    debug_assert!(self.rest().get(n..).is_some());
+    Output {
+      value: (),
+      rest: self.rest().get_unchecked(n..),
+    }
+  }
 }
 
 impl<'text, Value> Output<'text, Value> {
@@ -22,6 +49,47 @@ impl<'text, Value> Output<'text, Value> {
 #[cfg(test)]
 mod tests {
   use super::*;
+
+  #[test]
+  fn input_digest() {
+    let mut state = ();
+    let mut heap = ();
+    let input = Input::new("123", 0, &mut state, &mut heap).unwrap();
+    assert_eq!(input.digest(3).map(|output| output.rest), Some(""));
+    assert_eq!(input.digest(2).map(|output| output.rest), Some("3"));
+    assert_eq!(input.digest(1).map(|output| output.rest), Some("23"));
+    assert_eq!(input.digest(0).map(|output| output.rest), Some("123"));
+    assert!(input.digest(4).is_none());
+  }
+
+  #[test]
+  fn input_digest_unchecked() {
+    let mut state = ();
+    let mut heap = ();
+    let input = Input::new("123", 0, &mut state, &mut heap).unwrap();
+    assert_eq!(unsafe { input.digest_unchecked(3).rest }, "");
+    assert_eq!(unsafe { input.digest_unchecked(2).rest }, "3");
+    assert_eq!(unsafe { input.digest_unchecked(1).rest }, "23");
+    assert_eq!(unsafe { input.digest_unchecked(0).rest }, "123");
+  }
+
+  #[test]
+  #[should_panic]
+  fn input_digest_unchecked_overflow() {
+    let mut state = ();
+    let mut heap = ();
+    let input = Input::new("123", 0, &mut state, &mut heap).unwrap();
+    unsafe { input.digest_unchecked(4) };
+  }
+
+  #[test]
+  #[should_panic]
+  fn input_digest_unchecked_invalid_code_point() {
+    let mut state = ();
+    let mut heap = ();
+    let input = Input::new("好", 0, &mut state, &mut heap).unwrap();
+    unsafe { input.digest_unchecked(1) };
+  }
 
   #[test]
   fn output_map() {
