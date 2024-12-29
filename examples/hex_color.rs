ccd40f9a1e29d9e6282542f9940a04c32a64a735
DiscreteTom <discrete_tom@outlook.com>
2024-12-29 20:06:54 +0800

    chore: add examples/hex_color

---
+++ examples/hex_color.rs
@@ -0,0 +1,25 @@
+use whitehole::{
+  combinator::{eat, next},
+  parser::Builder,
+};
+
+fn main() {
+  let double_hex = || {
+    // Repeat a combinator with `*`.
+    (next(|c| c.is_ascii_hexdigit()) * 2)
+      // Convert the matched content to `u8`.
+      .select(|ctx| u8::from_str_radix(ctx.content(), 16).unwrap())
+      // Wrap `u8` to `(u8,)`, this is required by `+` below.
+      .tuple()
+  };
+
+  // Concat multiple combinators with `+`.
+  // Tuple values will be concatenated into a single tuple.
+  // Here `() + (u8,) + (u8,) + (u8,)` will be `(u8, u8, u8)`.
+  let entry = eat('#') + double_hex() + double_hex() + double_hex();
+
+  let mut parser = Builder::new().entry(entry).build("#FFA500");
+  let output = parser.parse().unwrap();
+  assert_eq!(output.digested, 7);
+  assert_eq!(output.value, (255, 165, 0));
+}
