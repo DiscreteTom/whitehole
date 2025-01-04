02da361e0e2ee56e9393552c7155beea432df252
DiscreteTom <discrete_tom@outlook.com>
2025-01-04 21:09:09 +0800

    chore: update examples/regex

--- examples/regex.rs
+++ examples/regex.rs
@@ -1,14 +1,13 @@
 use regex::Regex;
-use whitehole::{combinator::eater_unchecked, C};
+use whitehole::{combinator::wrap_unchecked, C};
 
 /// Create a combinator from a regex.
 pub fn regex<State, Heap>(s: &str) -> C!((), State, Heap) {
   let re = Regex::new(s).unwrap();
   unsafe {
-    eater_unchecked(move |input| {
+    wrap_unchecked(move |input| {
       re.find(input.instant().rest())
-        .map(|m| m.len())
-        .unwrap_or(0)
+        .map(|m| input.digest_unchecked(m.len()))
     })
   }
 }
