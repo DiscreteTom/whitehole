01fffecd0b542efa47cdbc00b30fd2ff9afe030f
DiscreteTom <discrete_tom@outlook.com>
2025-01-31 17:04:13 +0800

    docs: test README without include it as the crate doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
-#![doc = include_str!("../README.md")]
-
 pub mod action;
 pub mod combinator;
 pub mod digest;
 pub mod instant;
 pub mod parser;
 pub mod range;
+
+#[cfg(doctest)]
+#[doc = include_str!("../README.md")]
+mod readme {}
